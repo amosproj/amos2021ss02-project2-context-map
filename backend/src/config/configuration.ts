9f1f1238474c4b84b109158abce4310a9ba04b93
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2021-05-07 14:01:48 +0200

    changed backend to nest

---
+++ backend/src/config/configuration.ts
@@ -0,0 +1,12 @@
+/**
+ * Configuration options.
+ * Maps environment variables to object.
+ */
+export const configuration = () => ({
+  'neo4j.scheme': process.env.NEO4J_SCHEME,
+  'neo4j.host': process.env.NEO4J_HOST,
+  'neo4j.port': process.env.NEO4J_PORT,
+  'neo4j.username': process.env.NEO4J_USERNAME,
+  'neo4j.password': process.env.NEO4J_PASSWORD,
+  'neo4j.database': process.env.NEO4J_DATABASE,
+});
