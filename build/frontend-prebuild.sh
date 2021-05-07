511537ad1d2b5f6973b283243b6cd3436796ac82
Cato Léan Trütschel <cato@truetschel.de>
2021-05-07 16:32:59 +0200

    Add remark in batch file to keep it executable

--- build/frontend-prebuild.sh
+++ build/frontend-prebuild.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# Please make sure that this file is executable if you are working in a windows environment
+# This can be done by running "git update-index --chmod=+x .\frontend-prebuild.sh" in the build directory
+
 # Copy shared files
 cp -a ../shared/src/. ../frontend/src/shared/
