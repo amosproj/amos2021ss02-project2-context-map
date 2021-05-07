de2a1175745f226623ddfa3e0e6b4d2c178ee57d
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2021-05-07 16:54:10 +0200

    added ParseIntArrayPipe

---
+++ backend/src/pipes/ParseIntArrayPipe.spec.ts
@@ -0,0 +1,37 @@
+import { ParseIntArrayPipe } from './ParseIntArrayPipe';
+import { ArgumentMetadata } from '@nestjs/common';
+
+describe('ParseIntArrayPipe', () => {
+  let pipe = new ParseIntArrayPipe();
+  let metadata: ArgumentMetadata = {
+    type: 'query',
+  };
+
+  describe('single value input', () => {
+    it('should return int array when called with int', async () => {
+      await expect(pipe.transform(1, metadata)).resolves.toStrictEqual([1]);
+    });
+
+    it('should return int array when called with string', async () => {
+      await expect(pipe.transform('2', metadata)).resolves.toStrictEqual([2]);
+    });
+  });
+
+  describe('array input', () => {
+    it('should return int array when called with int  array', async () => {
+      await expect(
+        pipe.transform([1, 2, 3, 4], metadata),
+      ).resolves.toStrictEqual([1, 2, 3, 4]);
+    });
+
+    it('should return int array when called with string array', async () => {
+      await expect(
+        pipe.transform(['1', '2', '3'], metadata),
+      ).resolves.toStrictEqual([1, 2, 3]);
+    });
+  });
+
+  it('should throw an Error when called wrong', async () => {
+    await expect(pipe.transform(true, metadata)).rejects.toThrowError();
+  });
+});
