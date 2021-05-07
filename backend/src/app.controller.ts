54f049ec4a7705fa680047a2ee662c5a26d0ab89
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2021-05-07 21:35:23 +0200

    #61 created rest endpoints for querying nodes and edges

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Query } from "@nestjs/common";
-import { Neo4jService } from "nest-neo4j/dist";
-import { Node } from "./entities/Node";
-import { Edge } from "./entities/Edge";
-import { DetailedEdge } from "./entities/DetailedEdge";
+import { Controller, Get, Query } from '@nestjs/common';
+import { Neo4jService } from 'nest-neo4j/dist';
+import { Node } from './entities/Node';
+import { Edge } from './entities/Edge';
+import { DetailedEdge } from './entities/DetailedEdge';
+import { ParseIntArrayPipe } from './pipes/ParseIntArrayPipe';
 
 /**
  * Main App Controller.
@@ -10,67 +11,62 @@ import { DetailedEdge } from "./entities/DetailedEdge";
  */
 @Controller()
 export class AppController {
-  constructor(private readonly neo4jService: Neo4jService) {
-  }
+  constructor(private readonly neo4jService: Neo4jService) {}
 
   /**
    * Returns a list the ids of all nodes
    */
-  @Get("getAllNodes")
+  @Get('getAllNodes')
   async getAllNodes(): Promise<number[]> {
-    const result = await this.neo4jService.read("MATCH (n) RETURN ID(n) as id");
-
-    const ids: number[] = [];
-    result.records.forEach(function(record) {
-      // .toNumber() otherwise it returns { "low": id, "high": 0 }
-      ids.push(record.get("id").toNumber());
-    });
-
-    return ids;
+    const result = await this.neo4jService.read('MATCH (n) RETURN ID(n) as id');
+    return result.records.map((x) => x.toObject().id as number);
   }
 
   /**
    * Returns list of Nodes
-   * TODO Use a pipe to map query-params to array of numbers (currently string(s))
    */
-  @Get("getNodeDetails")
-  async getNodeDetails(@Query("ids") ids: number[]): Promise<Node[]> {
+  @Get('getNodeDetails')
+  async getNodeDetails(
+    @Query('ids', ParseIntArrayPipe) ids: number[],
+  ): Promise<Node[]> {
     const result = await this.neo4jService.read(
-      "MATCH (n) WHERE ID(n) IN [2, 4] RETURN ID(n) as id, labels(n) as labels, properties(n) as properties",
-      { ids }
+      'MATCH (n) WHERE ID(n) IN $ids RETURN ID(n) as id, labels(n) as labels, properties(n) as properties',
+      { ids },
     );
 
-    const nodes: Node[] = [];
-    result.records.forEach(function(record) {
-      nodes.push({
-        id: record.get("id").toNumber(), labels: record.get("labels"),
-        properties: Object.keys(record.get("properties")).reduce(function(filtered, key) {
-          // TODO: convert { "low": id, "high": 0 } to number
-          filtered[key] = record.get("properties")[key];
-          return filtered;
-        }, {})
-      });
-    });
-
-    return nodes;
+    return result.records.map((record) => record.toObject() as Node);
   }
 
   /**
    * Returns a list the ids of all edges
    */
-  @Get("getAllEdges")
+  @Get('getAllEdges')
   async getAllEdges(): Promise<Edge[]> {
-    // TODO return the IDs
-    return [];
+    const result = await this.neo4jService.read(`
+      MATCH (from)-[e]-(to) 
+      RETURN ID(e) as id, ID(from) as from, ID(to) as to
+    `);
+    return result.records.map((r) => r.toObject() as Edge);
   }
 
   /**
    * Returns list of detailed edges
-   * TODO Use a pipe to map query-params to array of numbers (currently string(s))
+   *
+   * @example call it with /getEdgeDetails?ids=1&ids=2
    */
-  @Get("getEdgeDetails")
-  async getEdgeDetails(@Query("ids") ids: number[]): Promise<DetailedEdge[]> {
-    // TODO return objects
-    return [];
+  @Get('getEdgeDetails')
+  async getEdgeDetails(
+    @Query('ids', ParseIntArrayPipe) ids: number[],
+  ): Promise<DetailedEdge[]> {
+    const result = await this.neo4jService.read(
+      `
+      MATCH (from)-[e]-(to) 
+      WHERE ID(e) in $ids
+      RETURN ID(e) as id, ID(from) as from, ID(to) as to, properties(e) as properties, type(e) as type
+    `,
+      { ids },
+    );
+
+    return result.records.map((r) => r.toObject() as DetailedEdge);
   }
 }
