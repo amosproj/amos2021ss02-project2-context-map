ea6c217b243b52ddec4e91a11053f509993ec80d
PianoRollRepresentation <kopytotobias@gmail.com>
2021-05-09 19:49:32 +0200

    added unit-tests for app.controller.ts

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -1,6 +1,12 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { QueryResult } from "./entities/queries/QueryResult";
+import { TestingDumpData } from "./fixtures/testingDumpData";
+import { Neo4jModule} from "nest-neo4j/dist";
+import { AppModule } from "./app.module";
+import { Node } from "./entities/Node";
+import { Edge } from "./entities/Edge";
 
 describe("AppController", () => {
   let appController: AppController;
@@ -8,19 +14,46 @@ describe("AppController", () => {
 
   beforeEach(async () => {
 
-    const app: TestingModule = await Test.createTestingModule({
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [AppModule, Neo4jModule],
       controllers: [AppController],
       providers: [AppService]
     }).compile();
 
-    appController = app.get<AppController>(AppController);
-    appService = app.get<AppService>(AppService);
+    appController = moduleRef.get<AppController>(AppController);
+    appService = moduleRef.get<AppService>(AppService);
   });
 
-  // TODO Create tests
-  describe("root", () => {
-    it("should return list of nodes corresponding to ids!", async () => {
+  describe("queryAll", () => {
+    it("should return edge and node ids (and from and to for edges)", async () => {
+      const result: QueryResult = {
+        nodes: TestingDumpData.nodeIds,
+        edges: TestingDumpData.edgeIds
+      };
 
+      jest.spyOn(appService, 'queryAll').mockImplementation(() => Promise.resolve(result));
+
+      expect(await appController.queryAll(TestingDumpData.limitQuery)).toBe(result);
+    });
+  });
+
+  describe("getNodesById", () => {
+    it("should return nodes", async () => {
+      const result: Node[] = TestingDumpData.nodes;
+
+      jest.spyOn(appService, 'getNodesById').mockImplementation(() => Promise.resolve(result));
+
+      expect(await appController.getNodesById([1, 2, 3])).toBe(result);
+    });
+  });
+
+  describe("getEdgesById", () => {
+    it("should return nodes", async () => {
+      const result: Edge[] = TestingDumpData.edges;
+
+      jest.spyOn(appService, 'getEdgesById').mockImplementation(() => Promise.resolve(result));
+
+      expect(await appController.getEdgesById([1, 2])).toBe(result);
     });
   });
 });
