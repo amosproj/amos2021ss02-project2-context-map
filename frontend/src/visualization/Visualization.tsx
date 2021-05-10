da640ead3ae32b90bade9dcf0694b3e71747834a
derwehr <derwehr@gmail.com>
2021-05-10 14:42:05 +0200

    implemented dashboard draft

--- frontend/src/visualization/Visualization.tsx
+++ frontend/src/visualization/Visualization.tsx
@@ -1,10 +1,39 @@
+import { Box, Container, Grid } from '@material-ui/core';
 import React from 'react';
+import routes from '../routing/routes';
+import DashboardCard from './DashboardCard';
 
 function Visualization(): JSX.Element {
+  const { tabs } = routes.Visualization;
+
+  if (tabs === undefined) {
+    throw new Error('Cannot read Visualization tabs');
+    return <></>;
+  }
   return (
     <>
-      <h1>Visualization</h1>
-      <p>TODO: Implement me, pls</p>
+      <h1>Visualization Dashboard</h1>
+      <Box>
+        <Container maxWidth={false}>
+          <Box>
+            <Grid container spacing={3}>
+              {tabs.map((tab) => (
+                <Grid item key={tab.label} lg={4} md={6} xs={12}>
+                  <a href={tab.path}>
+                    <DashboardCard
+                      path={tab.path}
+                      label={tab.label}
+                      content={tab.content}
+                      exact={tab.exact}
+                    />
+                  </a>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
+          <Box />
+        </Container>
+      </Box>
     </>
   );
 }
