9aed6aba6983180833d39bb123b4aa8457240f58
Igor Shmelev <igor.shmelev@hotmail.com>
2021-05-10 23:46:15 +0200

    Created CardDefinition and CardPresentationContent for defining dashboard card content based on route tabs

---
+++ frontend/src/visualization/dashboard-card/CardDefinition.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import TabDefinition from '../../routing/TabDefinition';
+
+export default interface CardDefinition extends TabDefinition{
+  label: string;
+  subLabel: string;
+  description: string;
+  icon: string;
+}
---
+++ frontend/src/visualization/dashboard-card/CardPresentationContent.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default interface CardPresentationContent {
+  label: string;
+  subLabel: string;
+  description: string;
+  icon?: string | undefined;
+}
