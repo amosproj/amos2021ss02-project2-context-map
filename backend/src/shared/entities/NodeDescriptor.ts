4a34f033084aa8e073a257fd96f36c642eedd6f2
Cato Léan Trütschel <cato@truetschel.de>
2021-05-11 17:48:36 +0200

    Delete shared files

--- backend/src/shared/entities/NodeDescriptor.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Describes a simple node of a graph.
- */
-export interface NodeDescriptor {
-  /**
-   * ID of the node
-   */
-  id: number;
-}
--- backend/src/shared/entities/Property.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Types that are possible for the properties of a neo4j node or relationship
- * TODO Find out which types are possible, see issue #85
- */
-export type Property = number | string | Record<string, unknown> | unknown;
