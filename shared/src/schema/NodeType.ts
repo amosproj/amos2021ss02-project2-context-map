8c02d46fb7604e2aa9076ce91813b300ce7fe317
Cato Léan Trütschel <cato@truetschel.de>
2021-05-14 14:42:46 +0200

    Steal shared types from #91

---
+++ shared/src/schema/NodeType.ts
@@ -0,0 +1,7 @@
+import { EntityType } from './EntityType';
+
+/**
+ * Represents a type of a node in a graph
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface NodeType extends EntityType {}
