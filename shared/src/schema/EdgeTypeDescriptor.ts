1492fb9f9d2a8ceae8afd9d5903d2f4a13956aa7
Cato Léan Trütschel <cato@truetschel.de>
2021-05-16 18:19:54 +0200

    Add schema entity type descriptors

---
+++ shared/src/schema/EdgeTypeDescriptor.ts
@@ -0,0 +1,7 @@
+import { EntityTypeDescriptor } from './EntityTypeDescriptor';
+
+/**
+ * A descriptor of an edge type.
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface EdgeTypeDescriptor extends EntityTypeDescriptor {}
---
+++ shared/src/schema/NodeTypeDescriptor.ts
@@ -0,0 +1,7 @@
+import { EntityTypeDescriptor } from './EntityTypeDescriptor';
+
+/**
+ * A descriptor of a node type.
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface NodeTypeDescriptor extends EntityTypeDescriptor {}
