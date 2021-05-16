246a382be483a9ac23a64471d73bec783f3e3d96
PianoRollRepresentation <kopytotobias@gmail.com>
2021-05-16 12:04:09 +0200

    Frontend use boxShadow-Array for every Entity-element in Graph.tsx

--- frontend/src/visualization/Graph.tsx
+++ frontend/src/visualization/Graph.tsx
@@ -7,22 +7,7 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import Button from '@material-ui/core/Button';
 import CloseIcon from '@material-ui/icons/Close';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-import {
-  Box,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  IconButton,
-  ListItemText,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from '@material-ui/core';
+import { Box, IconButton, ListItemText } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import TuneIcon from '@material-ui/icons/Tune';
 import useService from '../dependency-injection/useService';
@@ -35,7 +20,8 @@ import {
   CancellationTokenSource,
 } from '../utils/CancellationToken';
 import { useSize } from '../utils/useSize';
-import EntityComponent from '../components/EntityComponent';
+import EntityFilterElement from './components/EntityFilterElement';
+import EntityFilterDialog from './components/EntityFilterDialog';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -169,8 +155,38 @@ function Graph(): JSX.Element {
     new CancellationTokenSource()
   );
 
+  const nodeColorsAndTypes = [
+    { color: '#e6194b', type: 'Node Type 1' },
+    { color: '#3cb44b', type: 'Node Type 2' },
+    { color: '#ffe119', type: 'Node Type 3' },
+    { color: '#4363d8', type: 'Node Type 4' },
+    { color: '#f58231', type: 'Node Type 5' },
+    { color: '#911eb4', type: 'Node Type 6' },
+    { color: '#46f0f0', type: 'Node Type 7' },
+  ];
+  const nodeTypes: unknown[] = [];
+
+  const boxShadowStates: {
+    boxShadow: string;
+    setBoxShadow: React.Dispatch<React.SetStateAction<string>>;
+    handleAddNode: () => void;
+  }[] = [];
+
+  nodeColorsAndTypes.forEach(() => {
+    const [boxShadow, setBoxShadow] = React.useState('None');
+    const handleAddNode = () => {
+      setBoxShadow(
+        boxShadow === 'None' ? '0 0 0 0.2rem rgba(0,123,255,.5)' : 'None'
+      );
+    };
+    boxShadowStates.push({
+      boxShadow,
+      setBoxShadow,
+      handleAddNode,
+    });
+  });
+
   const [filterOpen, setFilterOpen] = React.useState(false);
-  const [boxShadow, setBoxShadow] = React.useState('None');
 
   // The state, as returned by react-async. Data is the query-result, when available.
   const { data, error, isLoading } = useAsync({
@@ -232,23 +248,22 @@ function Graph(): JSX.Element {
   // Build the react-graph-vis graph options.
   const options = buildOptions(containerSize.width, containerSize.height);
 
-  const handleAddNodes = () => {
-    setBoxShadow(
-      boxShadow === 'None' ? '0 0 0 0.2rem rgba(0,123,255,.5)' : 'None'
-    );
-  };
   const handleOpenFilter = () => {
     setFilterOpen(true);
   };
-
   const handleCloseFilter = () => {
     setFilterOpen(false);
   };
 
-  const entityTemplate = (color: string, type: string) => (
+  const entityTemplate = (
+    color: string,
+    type: string,
+    boxShadow: string,
+    handleAddNodes: () => void
+  ) => (
     <div className={classes.entityContainer}>
       <Box display="flex" p={1}>
-        <EntityComponent
+        <EntityFilterElement
           backgroundColor={color}
           boxShadow={boxShadow}
           content={type}
@@ -265,19 +280,17 @@ function Graph(): JSX.Element {
     </div>
   );
 
-  const nodeColorsAndTypes = [
-    { color: '#e6194b', type: 'Node Type 1' },
-    { color: '#3cb44b', type: 'Node Type 2' },
-    { color: '#ffe119', type: 'Node Type 3' },
-    { color: '#4363d8', type: 'Node Type 4' },
-    { color: '#f58231', type: 'Node Type 5' },
-    { color: '#911eb4', type: 'Node Type 6' },
-    { color: '#46f0f0', type: 'Node Type 7' },
-  ];
-  const nodeTypes: unknown[] = [];
-
+  let i = 0;
   nodeColorsAndTypes.forEach((colorsAndTypes) => {
-    nodeTypes.push(entityTemplate(colorsAndTypes.color, colorsAndTypes.type));
+    nodeTypes.push(
+      entityTemplate(
+        colorsAndTypes.color,
+        colorsAndTypes.type,
+        boxShadowStates[i].boxShadow,
+        boxShadowStates[i].handleAddNode
+      )
+    );
+    i += 1;
   });
 
   const edgeColorsAndTypes = [
@@ -287,26 +300,15 @@ function Graph(): JSX.Element {
   const edgeTypes: unknown[] = [];
 
   edgeColorsAndTypes.forEach((colorsAndTypes) => {
-    edgeTypes.push(entityTemplate(colorsAndTypes.color, colorsAndTypes.type));
+    edgeTypes.push(
+      entityTemplate(
+        colorsAndTypes.color,
+        colorsAndTypes.type,
+        boxShadowStates[0].boxShadow,
+        boxShadowStates[0].handleAddNode
+      )
+    );
   });
-
-  function createData(
-    name: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    protein: number
-  ) {
-    return { name, calories, fat, carbs, protein };
-  }
-
-  const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-  ];
 
   return (
     <>
@@ -318,45 +320,10 @@ function Graph(): JSX.Element {
           {edgeTypes}
         </div>
         <div>
-          <Dialog open={filterOpen} onClose={handleCloseFilter} scroll="paper">
-            <DialogTitle id="scroll-dialog-title">Filter Entity</DialogTitle>
-            <DialogContent>
-              <TableContainer component={Paper}>
-                <Table aria-label="simple table">
-                  <TableHead>
-                    <TableRow>
-                      <TableCell>Dessert (100g serving)</TableCell>
-                      <TableCell align="right">Calories</TableCell>
-                      <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                      <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                      <TableCell align="right">Protein&nbsp;(g)</TableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {rows.map((row) => (
-                      <TableRow key={row.name}>
-                        <TableCell component="th" scope="row">
-                          {row.name}
-                        </TableCell>
-                        <TableCell align="right">{row.calories}</TableCell>
-                        <TableCell align="right">{row.fat}</TableCell>
-                        <TableCell align="right">{row.carbs}</TableCell>
-                        <TableCell align="right">{row.protein}</TableCell>
-                      </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleCloseFilter} color="primary">
-                Cancel
-              </Button>
-              <Button onClick={handleCloseFilter} color="primary">
-                Apply Filter
-              </Button>
-            </DialogActions>
-          </Dialog>
+          <EntityFilterDialog
+            filterOpen={filterOpen}
+            handleCloseFilter={handleCloseFilter}
+          />
         </div>
         <div className={classes.graphContainer}>
           <div
