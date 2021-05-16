3a813388eb9806263cf3162315a58ced18383d18
Cato Léan Trütschel <cato@truetschel.de>
2021-05-16 11:35:54 +0200

    Add additional search and autosuggestion tests

--- backend/tests/e2e/search/search.service.spec.ts
+++ backend/tests/e2e/search/search.service.spec.ts
@@ -76,5 +76,113 @@ describe('SearchService', () => {
       // Assert
       expect(result).toEqual(expected);
     });
+
+    it('finds single edge by type', async () => {
+      // Arrange
+      const expected = {
+        edges: [
+          {
+            from: 3,
+            id: 2,
+            to: 0,
+          },
+        ],
+        nodes: [],
+      };
+
+      // Act
+      const result = await service.search('directed');
+
+      // Assert
+      expect(result).toEqual(expected);
+    });
+
+    it('finds single edge by property', async () => {
+      // Arrange
+      const expected = {
+        edges: [
+          {
+            from: 2,
+            id: 1,
+            to: 0,
+          },
+        ],
+        nodes: [],
+      };
+
+      // Act
+      const result = await service.search('trinity');
+
+      // Assert
+      expect(result).toEqual(expected);
+    });
+  });
+
+  describe('Method autosuggest', () => {
+    it('finds autosuggestion by edge property', async () => {
+      // Arrange
+      const expected = ['trinity'];
+
+      // Act
+      const result = await service.getAutoSuggestions('trin');
+
+      // Assert
+      expect(result).toEqual(expected);
+    });
+
+    it('finds autosuggestion by edge entity type', async () => {
+      // Arrange
+      const expected = ['edge'];
+
+      // Act
+      const result = await service.getAutoSuggestions('edg');
+
+      // Assert
+      expect(result).toEqual(expected);
+    });
+
+    it('finds autosuggestion by node entity type', async () => {
+      // Arrange
+      const expected = ['node'];
+
+      // Act
+      const result = await service.getAutoSuggestions('nod');
+
+      // Assert
+      expect(result).toEqual(expected);
+    });
+
+    it('finds autosuggestion by edge type', async () => {
+      // Arrange
+      const expected = ['directed'];
+
+      // Act
+      const result = await service.getAutoSuggestions('dir');
+
+      // Assert
+      expect(result).toEqual(expected);
+    });
+
+    it('finds autosuggestion by node type', async () => {
+      // Arrange
+      const expected = ['person'];
+
+      // Act
+      const result = await service.getAutoSuggestions('pers');
+
+      // Assert
+      expect(result).toEqual(expected);
+    });
+
+    it('finds autosuggestion by node property', async () => {
+      // Arrange
+      const expected = ['matrix'];
+
+      // Act
+      const result = await service.getAutoSuggestions('matr');
+
+      // Assert
+      expect(result).toEqual(expected);
+    });
   });
 });
