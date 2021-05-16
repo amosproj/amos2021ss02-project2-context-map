4ffba74b536cf6e35932ec5529721898a4fa8083
Cato Léan Trütschel <cato@truetschel.de>
2021-05-16 18:19:33 +0200

    Make linter happy

--- backend/src/pipes/RequiredPipe.ts
+++ backend/src/pipes/RequiredPipe.ts
@@ -9,7 +9,10 @@ import { HttpErrorByCode } from '@nestjs/common/utils/http-error-by-code.util';
 export class RequiredPipe implements PipeTransform {
   // eslint-disable-next-line class-methods-use-this
   transform<T>(value: T | undefined): T {
-    if (value == null || (typeof value === 'string' && value.trim().length === 0)) {
+    if (
+      value == null ||
+      (typeof value === 'string' && value.trim().length === 0)
+    ) {
       throw new HttpErrorByCode[HttpStatus.BAD_REQUEST](
         `Required argument missing`
       );
