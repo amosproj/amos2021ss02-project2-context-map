7eeb333e4c4ee9c16ba877b805628d64ec63c72d
derwehr <derwehr@gmail.com>
2021-05-17 15:49:15 +0200

    statisfy eslint

--- frontend/src/services/QueryServiceOptions.ts
+++ frontend/src/services/QueryServiceOptions.ts
@@ -2,9 +2,9 @@
  * Contains options for the query service.
  */
 export interface QueryServiceOptions {
-    /**
-     * The base URI of the backend to use.
-     * If not specified, uses the base URI of the source the frontend was loaded from.
-     */
-    backendBaseUri: string;
+  /**
+   * The base URI of the backend to use.
+   * If not specified, uses the base URI of the source the frontend was loaded from.
+   */
+  backendBaseUri: string;
 }
