564f322d7adb44c27652d02548f1b4f47aae489d
derwehr <derwehr@gmail.com>
2021-05-17 19:25:49 +0200

    updated base uri variable

--- build/start-frontend-dbg.sh
+++ build/start-frontend-dbg.sh
@@ -2,7 +2,7 @@
 frontendDir="$PWD/../frontend"
 frontendConfigPath="$frontendDir/.env"
 
-printf "REACT_APP_QUERY_SERVICE_BACKEND_BASE_URI=http://localhost:8080
+printf "REACT_APP_QUERY_SERVICE_BASE_URI=http://localhost:8080
 " > $frontendConfigPath
 
 yarn --cwd $frontendDir install
