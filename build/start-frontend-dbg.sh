e1a18b671f21bcef1d1ae4badce3f68ebdf85338
Thomas Wehr <derwehr@gmail.com>
2021-05-17 16:32:26 +0200

    changed >> to >

--- build/start-frontend-dbg.sh
+++ build/start-frontend-dbg.sh
@@ -3,7 +3,7 @@ frontendDir="$PWD/../frontend"
 frontendConfigPath="$frontendDir/.env"
 
 printf "REACT_APP_QUERY_SERVICE_BACKEND_BASE_URI=http://localhost:8080
-" >> $frontendConfigPath
+" > $frontendConfigPath
 
 yarn --cwd $frontendDir install
 yarn --cwd $frontendDir start
