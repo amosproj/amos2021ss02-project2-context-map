9dd58a6e367dc0cec577b2314c329f2a4efe7c31
Igor Shmelev <igor.shmelev@hotmail.com>
2021-05-18 22:25:20 +0200

    Rebuilding the searchbar to groups of entries

--- frontend/src/search/Searchbar.tsx
+++ frontend/src/search/Searchbar.tsx
@@ -1,32 +1,38 @@
 import React from 'react';
 import AsyncSelect from 'react-select/async';
-import { SearchResultEntry } from './SearchResultEntry';
 import useService from '../dependency-injection/useService';
 import SearchService from '../services/searchService';
-import convertSearchResultToEntries from './SearchEntryConverter';
+import { SearchResultEntryGroup } from './SearchResultEntryGroup';
+import convertSearchResultToEntryGroups from './SearchEntryConverter';
 
 export default function Searchbar(): JSX.Element {
+  /* eslint-disable-next-line  @typescript-eslint/no-explicit-any -- no use for the explicit type  */
   const customStyles = {
     option: (provided: any) => ({
       ...provided,
       color: 'black',
     }),
   };
-
   const searchService = useService(SearchService, null);
 
-  const loadOptions = function (
+  const searchEntries = function executeSearch(
     inputValue: string,
-    callback: (entries: SearchResultEntry[]) => void
+    callback: (entryGroups: SearchResultEntryGroup[]) => void
   ) {
     searchService
       .fullTextSearch(inputValue)
-      .then((result) => callback(convertSearchResultToEntries(result)));
-    // TODO Error handling for bad requests
+      .then((result) => callback(convertSearchResultToEntryGroups(result)))
+      // TODO what can we really do with the error here?
+      .catch((error) => error);
   };
 
-  /* eslint-disable react/jsx-no-bind */
+  /* eslint-disable react/jsx-no-bind -- simpler binding for stateless components  */
   return (
-    <AsyncSelect styles={customStyles} cacheOptions loadOptions={loadOptions} />
+    <AsyncSelect
+      styles={customStyles}
+      placeholder="Search nodes/edges/entities..."
+      cacheOptions
+      loadOptions={searchEntries}
+    />
   );
 }
