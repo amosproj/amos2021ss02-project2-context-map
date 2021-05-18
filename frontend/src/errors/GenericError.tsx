ddcf234789c2ac5ab9df2f310db021aaa4916959
derwehr <derwehr@gmail.com>
2021-05-19 00:48:16 +0200

    added error pages

---
+++ frontend/src/errors/GenericError.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+
+const useStyles = makeStyles({
+  root: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: '25px',
+  },
+
+  img: {
+    width: '25%',
+  },
+});
+
+function NotFoundError(): JSX.Element {
+  const classes = useStyles();
+  return (
+    <Box className={classes.root}>
+      <img src="//placehold.it/5000x3000" className={classes.img} alt="" />
+      <h1>Error</h1>
+      <p>An error occured</p>
+    </Box>
+  );
+}
+
+export default NotFoundError;
