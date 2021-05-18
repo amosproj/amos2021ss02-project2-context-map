3bc2bdd5ea3d4574b19a49153435ec4b74204b78
PianoRollRepresentation <kopytotobias@gmail.com>
2021-05-18 12:54:45 +0200

    Frontend Rearranged Add- and FilterButton to EntityFilterElement.tsx

--- frontend/src/visualization/components/EntityFilterElement.tsx
+++ frontend/src/visualization/components/EntityFilterElement.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import { IconButton } from '@material-ui/core';
+import TuneIcon from '@material-ui/icons/Tune';
+import AddIcon from '@material-ui/icons/Add';
+import EntityFilterDialog from './dialog/EntityFilterDialog';
 
 const useStyles = makeStyles({
   root: {
@@ -31,22 +35,52 @@ const useStyles = makeStyles({
 
 const EntityFilterElement = (props: {
   backgroundColor: string;
-  boxShadow: string;
-  content: string;
+  name: string;
+  entityTypePropertyNames: string[];
 }) => {
-  const { backgroundColor, boxShadow, content } = props;
+  // Indicates if filter-dialog is opened.
+  const [filterOpen, setFilterOpen] = React.useState(false);
+  const [boxShadow, setBoxShadow] = useState('None');
+
+  const { backgroundColor, name, entityTypePropertyNames } = props;
   const classes = useStyles();
 
+  const handleOpenFilter = () => {
+    setFilterOpen(true);
+  };
+  const handleCloseFilter = () => {
+    setFilterOpen(false);
+  };
+
+  const handleAddEntity = () => {
+    setBoxShadow(
+      boxShadow === 'None' ? '0 0 0 0.2rem rgba(0,123,255,.5)' : 'None'
+    );
+  };
+
   return (
-    <Button
-      style={{ backgroundColor, boxShadow }}
-      variant="contained"
-      color="primary"
-      disableRipple
-      className={classes.root}
-    >
-      {content}
-    </Button>
+    <div>
+      <Button
+        style={{ backgroundColor, boxShadow }}
+        variant="contained"
+        color="primary"
+        disableRipple
+        className={classes.root}
+      >
+        {name}
+      </Button>
+      <IconButton component="span">
+        <TuneIcon onClick={handleOpenFilter} />
+      </IconButton>
+      <IconButton component="span">
+        <AddIcon onClick={handleAddEntity} />
+      </IconButton>
+      <EntityFilterDialog
+        filterOpen={filterOpen}
+        handleCloseFilter={handleCloseFilter}
+        entityTypes={entityTypePropertyNames}
+      />
+    </div>
   );
 };
 
