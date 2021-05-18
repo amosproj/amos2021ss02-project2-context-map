b49612668abb913efd07ea24e535f4de39af896c
Igor Shmelev <igor.shmelev@hotmail.com>
2021-05-18 22:23:01 +0200

    Rebuild SearchResultEntry to fit for react-select options

--- frontend/src/search/SearchResultEntry.ts
+++ frontend/src/search/SearchResultEntry.ts
@@ -2,9 +2,12 @@
  * Represents a option element inside the dropdown of the autocomplete search.
  */
 export interface SearchResultEntry {
+  /**
+   * label that is visible to the user on the option item
+   */
   label: string,
+  /**
+   * value that corresponds to the option item
+   */
   value: string,
-  subLabel: string,
-  type: string,
-  color?: string | undefined
 }
