ea9e726a970b4c3819a989d7c85462a21c4e7dd3
derwehr <derwehr@gmail.com>
2021-05-18 10:06:44 +0200

    moved SearchService to dedicated service

--- frontend/src/services/FakeDataQueryService.ts
+++ frontend/src/services/FakeDataQueryService.ts
@@ -4,12 +4,9 @@ import { LimitQuery } from '../shared/queries/LimitQuery';
 import { QueryResult } from '../shared/queries/QueryResult';
 import { NodeDescriptor } from '../shared/entities/NodeDescriptor';
 import { EdgeDescriptor } from '../shared/entities/EdgeDescriptor';
-import { NodeTypeDescriptor } from '../shared/schema/NodeTypeDescriptor';
-import { EdgeTypeDescriptor } from '../shared/schema/EdgeTypeDescriptor';
 import delay from '../utils/delay';
 import QueryService from './QueryService';
 import { CancellationToken } from '../utils/CancellationToken';
-import { SearchResult } from '../shared/search/SearchResult';
 import getRandomInteger from '../utils/getRandomInteger';
 
 /**
@@ -57,49 +54,5 @@ export default class FakeDataQueryService extends QueryService {
     }
 
     return { nodes, edges };
-  }
-
-  public async fullTextSearch(
-    searchString: string,
-    path: string,
-    cancellation?: CancellationToken
-  ): Promise<SearchResult> {
-    await delay(0, cancellation);
-
-    const numNodes = path !== 'edges' ? 100 : 0;
-    const numEdges = path !== 'nodes' ? 150 : 0;
-    const nodes: NodeDescriptor[] = [];
-    const edges: EdgeDescriptor[] = [];
-    const nodeTypes: NodeTypeDescriptor[] = [
-      { name: 'Person' },
-      { name: 'Movie' },
-    ];
-    const edgeTypes: EdgeTypeDescriptor[] = [
-      { name: 'ACTED_IN' },
-      { name: 'DIRECTED' },
-    ];
-
-    for (let i = 0; i < numNodes; i += 1) {
-      nodes.push({ id: i });
-    }
-
-    for (let i = 0; i < numEdges; i += 1) {
-      const from = getRandomInteger(numNodes);
-      let to = getRandomInteger(numNodes);
-
-      if (!this.allowSelfReferencingNodes) {
-        while (to === from) {
-          to = getRandomInteger(numNodes);
-        }
-      }
-
-      edges.push({
-        id: i,
-        from,
-        to,
-      });
-    }
-
-    return { nodes, edges, nodeTypes, edgeTypes };
   }
 }
--- frontend/src/services/QueryService.ts
+++ frontend/src/services/QueryService.ts
@@ -2,7 +2,6 @@ import { injectable } from 'inversify';
 import 'reflect-metadata';
 import { LimitQuery } from '../shared/queries/LimitQuery';
 import { QueryResult } from '../shared/queries/QueryResult';
-import { SearchResult } from '../shared/search/SearchResult';
 import { CancellationToken } from '../utils/CancellationToken';
 
 /**
@@ -20,19 +19,4 @@ export default abstract class QueryService {
     query?: LimitQuery,
     cancellation?: CancellationToken
   ): Promise<QueryResult>;
-
-  /**
-   * Searches through all entities and entity types and returns those, that
-   * match the searchString. Only returns those values with the prefix of searchStrings.
-   * Values with n spaces are considered as n+1 single values.
-   * @param searchString The string to search for.
-   * @param path the path of the search API describes what to search for, can be 'all' 'nodes' or edges'.
-   * @param cancellation A CancellationToken used to cancel the asynchronous operation.
-   * @returns A promise that when evaluated, contains a {@link SearchResult} (nodes, edges, edgetypes and nodetypes).
-   */
-  public abstract fullTextSearch(
-    searchString: string,
-    path: string,
-    cancellation?: CancellationToken
-  ): Promise<SearchResult>;
 }
