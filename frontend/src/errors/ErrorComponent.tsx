12f13f510627300a82877446f332c44a1cb29660
derwehr <derwehr@gmail.com>
2021-05-25 16:42:02 +0200

    centered error component

--- frontend/src/errors/ErrorComponent.tsx
+++ frontend/src/errors/ErrorComponent.tsx
@@ -41,6 +41,7 @@ const ErrorComponentData = {
 const useStyles = makeStyles({
   root: {
     width: '100%',
+    height: '100%',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
