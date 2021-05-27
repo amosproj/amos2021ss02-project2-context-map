68ba2ea0f4ce83498683ca170859264508b46f02
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2021-05-27 13:24:04 +0200

    ♻ added ts barrels for shared types. closes #149

---
+++ shared/src/schema/index.ts
@@ -0,0 +1,9 @@
+export type { EdgeType } from './EdgeType';
+export type { EdgeTypeDescriptor } from './EdgeTypeDescriptor';
+
+export type { EntityType } from './EntityType';
+export type { EntityTypeDescriptor } from './EntityTypeDescriptor';
+export type { EntityTypeProperty } from './EntityTypeProperty';
+
+export type { NodeType } from './NodeType';
+export type { NodeTypeDescriptor } from './NodeTypeDescriptor';
