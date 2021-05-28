551bf910c28960a7134a2b8701c4bf0d6cff89a5
Cato Léan Trütschel <cato@truetschel.de>
2021-05-28 14:37:20 +0200

    Move frontend script files

--- build/frontend-prebuild.ps1
+++
@@ -1,9 +0,0 @@
-# Goto the frontend dir
-cd ../frontend
-
-# Copy shared files
-New-Item -ItemType Directory -Force -Path ".\src\shared"
-xcopy /S /I /Q /Y /F "..\shared\src" ".\src\shared"
-
-# Execute linter
-yarn lint
