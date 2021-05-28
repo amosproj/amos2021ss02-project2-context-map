07cbed4a3895286e34452a9a4aea20478336abf4
Cato Léan Trütschel <cato@truetschel.de>
2021-05-28 14:00:22 +0200

    Hopefully fix backend build

--- backend/scripts/backend-prebuild.sh
+++ backend/scripts/backend-prebuild.sh
@@ -8,9 +8,7 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
 
 # Only execute if not running inside a docker container
-grep -sq 'docker\|lxc' /proc/1/cgroup
-if [ $? -ne 0 ]; then
-
+if [[ ${DOCKER} != DOCKER ]]; then
     # Copy shared files
     cp -a ../shared/src/. ./src/shared/
 
