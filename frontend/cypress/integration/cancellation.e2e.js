0a396054e10ae7dbe9c74d9ffe023921ef6365e1
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2021-06-01 00:42:59 +0200

    updated cancellation test to match new graph implementation

--- frontend/cypress/integration/cancellation.e2e.js
+++ frontend/cypress/integration/cancellation.e2e.js
@@ -17,6 +17,27 @@ context('Filter', () => {
         loadingIconFound.resolve();
       });
 
-    cy.contains('Cancellation Error');
+    // wait until loading is done
+    cy.get('.MuiBackdrop-root').should('be.visible');
+
+    // get visible canvas
+    cy.get('.vis-network canvas')
+      .should('be.visible')
+      .then((jQueryCanvas) => {
+        // make sure the "canvas" really is a canvas
+        const canvas = jQueryCanvas.get()[0];
+        expect(canvas).to.be.instanceof(HTMLCanvasElement);
+        if (canvas instanceof HTMLCanvasElement) {
+          /// if (...) to enable intellisense
+          const imageData = canvas
+            .getContext('2d')
+            .getImageData(0, 0, canvas.width, canvas.height);
+          // check if all pixels are white (color === 0)
+          expect(imageData.data.filter((color) => color !== 0)).length(0);
+        } else {
+          /// will be caught by instanceof check, but just to be sure
+          expect(1).to.eq(0);
+        }
+      });
   });
 });
