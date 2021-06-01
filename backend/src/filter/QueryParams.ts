0b9fd6081f86755e0e87a9692e013431c1100ae9
Cato Léan Trütschel <cato@truetschel.de>
2021-06-01 12:18:40 +0200

    Add TSDoc to QueryParams

--- backend/src/filter/QueryParams.ts
+++ backend/src/filter/QueryParams.ts
@@ -1,3 +1,9 @@
+/**
+ * Represents the parameter of a database query.
+ */
 export interface QueryParams {
+  /**
+   * Gets or sets a database parameter by its key.
+   */
   [key: string]: unknown | undefined;
 }
