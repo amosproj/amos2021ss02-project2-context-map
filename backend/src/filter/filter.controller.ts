fc3d5f8256cc86c29a50685733df2f841f68d515
Cato Léan Trütschel <cato@truetschel.de>
2021-06-01 14:40:43 +0200

    Replace custom filter controller arg check with generic solution

--- backend/src/filter/filter.controller.ts
+++ backend/src/filter/filter.controller.ts
@@ -47,15 +47,8 @@ export class FilterController {
    */
   @Get('node-type')
   getNodeTypeFilterModel(
-    @Query('type') type: string
+    @Query('type', RequiredPipe) type: string
   ): Promise<NodeTypeFilterModel> {
-    if (typeof type !== 'string' || type.length === 0) {
-      // TODO: Replace with much better solution:
-      // https://github.com/amosproj/amos-ss2021-project2-context-map/blob/0a972dc11e29ebe59343de48a497cdb0f98d5d94/backend/src/pipes/RequiredPipe.ts
-      // See also: https://github.com/amosproj/amos-ss2021-project2-context-map/issues/110
-      throw new BadRequestException();
-    }
-
     return this.filterService.getNodeTypeFilterModel(type);
   }
 
@@ -65,15 +58,8 @@ export class FilterController {
    */
   @Get('edge-type')
   getEdgeTypeFilterModel(
-    @Query('type') type: string
+    @Query('type', RequiredPipe) type: string
   ): Promise<NodeTypeFilterModel> {
-    if (typeof type !== 'string' || type.length === 0) {
-      // TODO: Replace with much better solution:
-      // https://github.com/amosproj/amos-ss2021-project2-context-map/blob/0a972dc11e29ebe59343de48a497cdb0f98d5d94/backend/src/pipes/RequiredPipe.ts
-      // See also: https://github.com/amosproj/amos-ss2021-project2-context-map/issues/110
-      throw new BadRequestException();
-    }
-
     return this.filterService.getEdgeTypeFilterModel(type);
   }
 }
