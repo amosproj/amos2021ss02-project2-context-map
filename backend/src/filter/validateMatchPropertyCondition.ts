9282a9c262970098a01d60993352d8f464b2f736
Cato Léan Trütschel <cato@truetschel.de>
2021-06-01 12:53:06 +0200

    Move validate match property filter condition to place of usage

--- backend/src/filter/validateMatchPropertyCondition.ts
+++ backend/src/filter/validateMatchPropertyCondition.ts
@@ -1,6 +1,5 @@
 import { ArgumentError } from '../shared/errors';
 import { FilterCondition, MatchPropertyCondition } from '../shared/queries';
-import { checkValidMatchPropertyCondition } from './checkValidMatchPropertyCondition';
 import { formatErrorMessage } from './formatErrorMessage';
 
 /**
@@ -12,15 +11,16 @@ export function validateMatchPropertyCondition(
   condition: FilterCondition
 ): MatchPropertyCondition {
   const result = <MatchPropertyCondition>condition;
-  const validationResult = checkValidMatchPropertyCondition(result);
 
-  if (validationResult.invalidProperty !== undefined) {
+  if (result.property === undefined || typeof result.property !== 'string') {
     throw new ArgumentError(
-      formatErrorMessage(
-        'MatchPropertyCondition',
-        validationResult.invalidProperty,
-        validationResult.type
-      )
+      formatErrorMessage('MatchPropertyCondition', 'property', 'string')
+    );
+  }
+
+  if (result.value === undefined) {
+    throw new ArgumentError(
+      formatErrorMessage('MatchPropertyCondition', 'value')
     );
   }
 
