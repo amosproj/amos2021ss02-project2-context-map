471c51d4d1d072b243b0a6d6d5a400f123fe3d4a
Cato Léan Trütschel <cato@truetschel.de>
2021-06-01 13:47:28 +0200

    Add FilterServiceBase TSDoc

--- backend/src/filter/filter.service.base.ts
+++ backend/src/filter/filter.service.base.ts
@@ -1,13 +1,31 @@
 import { FilterQuery, QueryResult } from '../shared/queries';
 import { EdgeTypeFilterModel, NodeTypeFilterModel } from '../shared/filter';
 
+/**
+ * Base type for filter services.
+ */
 export abstract class FilterServiceBase {
+  /**
+   * Asynchronously performs a query with the specified query parameters.
+   * @param query The query parameters that specify the desired query result.
+   * @returns A promise that represents the asynchronous operation. When evaluated, the promise result contains the query result.
+   */
   public abstract query(query?: FilterQuery): Promise<QueryResult>;
 
+  /**
+   * Asynchronously gets a specialized model for the specified type of node that contains all information necessary to display a filter-line.
+   * @param type The type of node.
+   * @returns A promise that represents the asynchronous operation. When evaluated, the promise result contains the requested model.
+   */
   public abstract getNodeTypeFilterModel(
     type: string
   ): Promise<NodeTypeFilterModel>;
 
+  /**
+   * Asynchronously gets a specialized model for the specified type of edge that contains all information necessary to display a filter-line.
+   * @param type The type of edge.
+   * @returns A promise that represents the asynchronous operation. When evaluated, the promise result contains the requested model.
+   */
   public abstract getEdgeTypeFilterModel(
     type: string
   ): Promise<EdgeTypeFilterModel>;
