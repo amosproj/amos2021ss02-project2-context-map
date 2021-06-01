98e84c8fded58f8416d88043810e3535a6b55bbd
Cato Léan Trütschel <cato@truetschel.de>
2021-06-01 18:30:47 +0200

    Remove unused expanded search result type

--- shared/src/search/index.ts
+++ shared/src/search/index.ts
@@ -1,4 +1,3 @@
 export type { SearchResult } from './SearchResult';
-export type { ExpandedSearchResult } from './ExpandedSearchResult';
 export type { SearchNodeResult } from './SearchNodeResult';
 export type { SearchEdgeResult } from './SearchEdgeResult';
