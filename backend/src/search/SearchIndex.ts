03bcbf4ce57623e2db64c7695b5374b92b8bc3e1
Cato Léan Trütschel <cato@truetschel.de>
2021-06-02 09:43:59 +0200

    Refactor search index construction to search index type

--- backend/src/search/SearchIndex.ts
+++ backend/src/search/SearchIndex.ts
@@ -6,12 +6,87 @@ import {
   SearchResult,
 } from '../shared/search';
 import { IndexEntry } from './IndexEntry';
+import { SearchIndexEntryRecorder } from './SearchIndexEntryRecorder';
+import { AppService } from '../app.service';
+import { SchemaService } from '../schema/schema.service';
 
 /**
  * Represents a search index that can be used to perform search operations and auto suggestions.
  */
 export class SearchIndex {
-  public constructor(private readonly index: AsyncLazy<MiniSearch>) {}
+  private constructor(private readonly index: AsyncLazy<MiniSearch>) {}
+
+  public static create(
+    queryService: AppService,
+    schemaService: SchemaService
+  ): SearchIndex {
+    return new SearchIndex(
+      new AsyncLazy<MiniSearch>(() =>
+        SearchIndex.createCore(queryService, schemaService)
+      )
+    );
+  }
+
+  private static async createCore(
+    queryService: AppService,
+    schemaService: SchemaService
+  ): Promise<MiniSearch> {
+    const entryRecorder = new SearchIndexEntryRecorder();
+
+    // TODO: For large databases it would be beneficial, if we read the entries in smaller batches,
+    //       so we don't have to copy the entire dataset into memory (again).
+    //       The problem here, is that we need the property names before adding the entries to the
+    //       index. This can be done by executing an up-front query for the schema of the data,
+    //       as is done in the SchemaService.
+
+    // Get all nodes from the database
+    const nodes = await queryService.getNodes();
+
+    for (const node of nodes) {
+      entryRecorder.recordNode(node);
+    }
+
+    // Get all edges from the database
+    const edges = await queryService.getEdges();
+
+    for (const edge of edges) {
+      entryRecorder.recordEdge(edge);
+    }
+
+    // Get all node types from the database
+    const nodeTypes = await schemaService.getNodeTypes();
+
+    for (const nodeType of nodeTypes) {
+      entryRecorder.recordNodeType(nodeType);
+    }
+
+    // Get all edge types from the database
+    const edgeTypes = await schemaService.getEdgeTypes();
+
+    for (const edgeType of edgeTypes) {
+      entryRecorder.recordEdgeType(edgeType);
+    }
+
+    // Build the index
+    const fields = ['indexValue'];
+    const storeFields = [
+      'entityType',
+      'id',
+      'types',
+      'from',
+      'to',
+      'indexKey',
+      'indexValue',
+    ];
+
+    const index = new MiniSearch({
+      idField: 'id',
+      fields,
+      storeFields,
+    });
+    index.addAll(entryRecorder.recordedEntries);
+    return index;
+  }
 
   /**
    * Searches through all entities and entity types and returns those, that
--- backend/src/search/SearchIndexBuilder.ts
+++ backend/src/search/SearchIndexBuilder.ts
@@ -1,8 +1,5 @@
-import MiniSearch from 'minisearch';
 import { Injectable } from '@nestjs/common';
 import { SearchIndex } from './SearchIndex';
-import { AsyncLazy } from '../shared/utils';
-import { SearchIndexEntryRecorder } from './SearchIndexEntryRecorder';
 import { SchemaService } from '../schema/schema.service';
 import { AppService } from '../app.service';
 
@@ -21,64 +18,6 @@ export class SearchIndexBuilder {
    * @returns The constructed search index.
    */
   public buildIndex(): SearchIndex {
-    return new SearchIndex(new AsyncLazy(() => this.buildIndexCore()));
-  }
-
-  private async buildIndexCore(): Promise<MiniSearch> {
-    const entryRecorder = new SearchIndexEntryRecorder();
-
-    // TODO: For large databases it would be beneficial, if we read the entries in smaller batches,
-    //       so we don't have to copy the entire dataset into memory (again).
-    //       The problem here, is that we need the property names before adding the entries to the
-    //       index. This can be done by executing an up-front query for the schema of the data,
-    //       as is done in the SchemaService.
-
-    // Get all nodes from the database
-    const nodes = await this.queryService.getNodes();
-
-    for (const node of nodes) {
-      entryRecorder.recordNode(node);
-    }
-
-    // Get all edges from the database
-    const edges = await this.queryService.getEdges();
-
-    for (const edge of edges) {
-      entryRecorder.recordEdge(edge);
-    }
-
-    // Get all node types from the database
-    const nodeTypes = await this.schemaService.getNodeTypes();
-
-    for (const nodeType of nodeTypes) {
-      entryRecorder.recordNodeType(nodeType);
-    }
-
-    // Get all edge types from the database
-    const edgeTypes = await this.schemaService.getEdgeTypes();
-
-    for (const edgeType of edgeTypes) {
-      entryRecorder.recordEdgeType(edgeType);
-    }
-
-    // Build the index
-    const fields = ['indexValue'];
-    const storeFields = [
-      'entityType',
-      'id',
-      'types',
-      'from',
-      'to',
-      'indexKey',
-      'indexValue',
-    ];
-
-    const index = new MiniSearch({
-      idField: 'id',
-      fields,
-      storeFields,
-    });
-    index.addAll(entryRecorder.recordedEntries);
-    return index;
+    return SearchIndex.create(this.queryService, this.schemaService);
   }
 }
