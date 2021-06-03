9e91b7067341e3184e7a4dbd2fc7ca640603d0d6
Cato Léan Trütschel <cato@truetschel.de>
2021-06-03 15:52:59 +0200

    Enable neo4j gds plugin

--- build/start-database.ps1
+++ build/start-database.ps1
@@ -1,9 +1,10 @@
 $scriptRoot = $PSScriptRoot
 $mountDir = $scriptRoot + "\..\backend\docker\mount"
+$pluginsDir = $scriptRoot + "\..\backend\docker\plugins"
 $databaseDump = "testing-dump"
 $dbPassword = "amos"
 $containerName = "neo4j-db"
 
 docker container stop $containerName
 docker container rm $containerName
-docker run -p 7474:7474 -p 7687:7687 -v $mountDir`:/mnt/amos --env NEO4J_ACCEPT_LICENSE_AGREEMENT=yes --env DB_PASSWORD=$dbPassword --env DB_PATH=/mnt/amos/dumps/$databaseDump.dump --name $containerName neo4j:enterprise /mnt/amos/load-dump.sh
+docker run -p 7474:7474 -p 7687:7687 -v $mountDir`:/mnt/amos -v $pluginsDir`:/plugins --env NEO4J_ACCEPT_LICENSE_AGREEMENT=yes --env DB_PASSWORD=$dbPassword --env DB_PATH=/mnt/amos/dumps/$databaseDump.dump --env 'NEO4JLABS_PLUGINS=["apoc", "graph-algorithms", "gds"]' --env 'NEO4J_dbms_security_procedures_unrestricted=apoc.*,algo.*,gds.*' --env 'NEO4J_dbms_security_procedures_whitelist=apoc.*,algo.*,gds.*' --env NEO4J_apoc_import_file_enabled=true --env NEO4J_dbms_shell_enabled=true --name $containerName neo4j:enterprise /mnt/amos/load-dump.sh
