5746287167438e80930891cb6033008b3ac33578
Cato Léan Trütschel <cato@truetschel.de>
2021-06-03 13:17:26 +0200

    QueryNodeResults can be virtual

--- shared/src/queries/QueryNodeResult.ts
+++ shared/src/queries/QueryNodeResult.ts
@@ -8,4 +8,11 @@ export default interface QueryNodeResult extends NodeDescriptor {
    * A boolean that specifies whether the node is part of the result only to support edges that are part of the result that reference the node.
    */
   subsidiary?: boolean;
+
+  /**
+   * A boolean that specifies whether the node is virtual.
+   * A virtual node is a node that is not part of the dataset but is a placeholder for a complete subgraph.
+   * A virtual node's id is oblivious. It can be of any value and does not correspond to a true database entry.
+   */
+  virtual?: boolean;
 }
