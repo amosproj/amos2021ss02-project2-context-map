346023c27b103f5320882d6f15581a356280d0ba
Cato Léan Trütschel <cato@truetschel.de>
2021-06-03 11:32:12 +0200

    Add some missing doc to QueryBase

--- shared/src/queries/QueryBase.ts
+++ shared/src/queries/QueryBase.ts
@@ -1,6 +1,23 @@
+/**
+ * A base type for query objects.
+ */
 export default interface QueryBase {
+  /**
+   * A limits object that specifies the number maximum of nodes and edges to include in the query result.
+   */
   limits?: {
+    /**
+     * The maximum number of nodes to include in the query result.
+     * If a value of 0 is specified, the query result does not include any nodes.
+     * If the value is unspecified, the number of nodes is unlimited.
+     */
     nodes?: number;
+
+    /**
+     * The maximum number of edges to include in the query result.
+     * If a value of 0 is specified, the query result does not include any edges.
+     * If the value is unspecified, the number of edges is unlimited.
+     */
     edges?: number;
   };
 
