52432e2a2ad11199b5f2af1b61aeac3cd23ab9de
Cato Léan Trütschel <cato@truetschel.de>
2021-06-03 11:29:30 +0200

    Add a boolean parameter to query objects

--- shared/src/queries/QueryBase.ts
+++ shared/src/queries/QueryBase.ts
@@ -3,4 +3,10 @@ export default interface QueryBase {
     nodes?: number;
     edges?: number;
   };
+
+  /**
+   * A boolean value that determines whether subsidiary nodes will be included in the query result.
+   * If unspecified or false, edges that reference non-included nodes are removed from the query result.
+   */
+  includeSubsidiary?: boolean;
 }
