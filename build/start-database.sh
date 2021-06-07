0be928dbe5de2d962e1e804b3a48932d51571837
Cato Léan Trütschel <cato@truetschel.de>
2021-06-07 12:04:35 +0200

    Update unix database run script

--- build/start-database.sh
+++ build/start-database.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 mountDir="$PWD/../backend/docker/mount/"
+pluginsDir="$PWD/../backend/docker/plugins/"
 databaseDump="testing-dump"
 dbPassword="amos"
 containerName="neo4j-db"
@@ -11,9 +12,15 @@ docker container rm $containerName
 docker run \
     -p 7474:7474 -p 7687:7687 \
     -v $mountDir:/mnt/amos \
+    -v $pluginsDir:/plugins \
     --env NEO4J_ACCEPT_LICENSE_AGREEMENT=yes \
     --env DB_PASSWORD=$dbPassword \
     --env DB_PATH=/mnt/amos/dumps/$databaseDump.dump \
+    --env 'NEO4JLABS_PLUGINS=["apoc", "gds"]' \
+    --env 'NEO4J_dbms_security_procedures_unrestricted=apoc.*,gds.*' \
+    --env 'NEO4J_dbms_security_procedures_allowlist=apoc.*,gds.*' \
+    --env NEO4J_apoc_import_file_enabled=true \
+    --env NEO4J_dbms_shell_enabled=true \
     --name $containerName  \
     neo4j:enterprise  \
     /mnt/amos/load-dump.sh
