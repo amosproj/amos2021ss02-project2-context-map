60a84810f31bbc35e282cfe37fa4fbb06348bf52
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2021-06-07 11:19:05 +0200

    modifies color store to support subsidiaries

--- frontend/src/stores/colors/EntityVisualisationAttributes.ts
+++ frontend/src/stores/colors/EntityVisualisationAttributes.ts
@@ -2,5 +2,16 @@
  * Attributes used for visualisation of entities.
  */
 export default interface EntityVisualisationAttributes {
+  /**
+   * Background color
+   */
   color: string;
+}
+
+export interface NodeVisualisationAttributes
+  extends EntityVisualisationAttributes {
+  /**
+   * Border Attributes
+   */
+  border: { color: string };
 }
