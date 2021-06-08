bcdd82987aff14f5dec5e17f34375a8992082d69
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2021-06-08 18:56:28 +0200

    updates test for EntityColorizer as object

--- frontend/cypress/unit/stores/colors/EntityColorStore.test.ts
+++ frontend/cypress/unit/stores/colors/EntityColorStore.test.ts
@@ -18,21 +18,21 @@ describe('EntityColorStore', () => {
   });
 
   it('should return different colors for different entity types', () => {
-    const colorize = entityColorStore.getValue();
-    const color1 = colorize(dummies[0]);
-    const color2 = colorize(dummies[1]);
+    const colorizer = entityColorStore.getValue();
+    const color1 = colorizer.colorize(dummies[0]);
+    const color2 = colorizer.colorize(dummies[1]);
 
     expect(color1).not.to.be.deep.eq(color2);
   });
 
   it('should return grey for all node types when greyScale is true', () => {
-    const colorize = entityColorStore.getValue();
+    const colorizer = entityColorStore.getValue();
     entityColorStore.setGreyScale(true);
     const grey = '#9e9e9e';
     const colors = [];
-    colors.push(colorize(dummies[1]));
-    colors.push(colorize(dummies[2]));
-    colors.push(colorize(dummies[3]));
+    colors.push(colorizer.colorize(dummies[1]));
+    colors.push(colorizer.colorize(dummies[2]));
+    colors.push(colorizer.colorize(dummies[3]));
 
     for (const color of colors) {
       expect(color.color).to.be.eq(grey);
