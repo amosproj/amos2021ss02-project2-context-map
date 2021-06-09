d17c2bef635f02bb9c48b3c07afede59a56d16b1
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2021-06-09 16:48:55 +0200

    creates store for exploration feature (#291)

---
+++ frontend/src/stores/exploration/ExplorationQuestions.ts
@@ -0,0 +1,8 @@
+import { ExplorationQuestion } from './interfaces/ExplorationQuestion';
+
+/**
+ * Contains all available question.
+ * Must not be changed during runtime.
+ */
+const explorationQuestions: ReadonlyArray<ExplorationQuestion[]> = [];
+export default explorationQuestions;
