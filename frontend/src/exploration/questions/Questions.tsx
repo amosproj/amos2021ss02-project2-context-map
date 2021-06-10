66a0cbff4d83e1f3c675cb173bd6245ce1b8190e
derwehr <derwehr@gmail.com>
2021-06-10 23:27:33 +0200

    save selections in state

--- frontend/src/exploration/questions/Questions.tsx
+++ frontend/src/exploration/questions/Questions.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Accordion,
   AccordionDetails,
@@ -31,29 +31,43 @@ const useStyle = makeStyles((theme: Theme) =>
   })
 );
 
+// Interface to keep track of checked answers in component state.
+interface ExplorationSelection {
+  answer: ExplorationAnswer;
+  checked: boolean;
+}
+
+// Map answers to string made of questionIndex and answerIndex, in order to add and
+// remove them from the explorationStore in handleChange.
+const selections: Record<string, ExplorationSelection> = {};
+explorationQuestions.forEach((q, qIndex) => {
+  q.answers.forEach((a, aIndex) => {
+    selections[`explorationAnswer-${qIndex}-${aIndex}`] = {
+      answer: a,
+      checked: false,
+    };
+  });
+});
+
 function Questions(): JSX.Element {
   const classes = useStyle();
-
   const explorationStore = useService(ExplorationStore);
-
-  // Map answers to string of questionIndex-answerIndex, in order to add and remove
-  // them from the explorationStore in handleChange
-  const answers: Record<string, ExplorationAnswer> = {};
-  explorationQuestions.forEach((q, qIndex) => {
-    q.answers.forEach((a, aIndex) => {
-      answers[`explorationAnswer-${qIndex}-${aIndex}`] = a;
-    });
-  });
+  const [state, setState] = useState(selections);
 
   /**
    * Handles change event of the exploration questions,
    * by adding newly selected answers and
    * removing de-selected answers from the ExplorationStore.
-   * @param event the changeEvent
+   * @param event the changeEvent emitting this function.
    */
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name } = event.target;
-    const answer = answers[name];
+    const { name, checked } = event.target;
+    const { answer } = state[name];
+
+    setState({
+      ...state,
+      [name]: { answer, checked },
+    });
 
     if (event.target.checked) {
       explorationStore.addAnswer(answer);
@@ -83,8 +97,13 @@ function Questions(): JSX.Element {
                     <FormControlLabel
                       control={
                         <Checkbox
+                          checked={
+                            state[`explorationAnswer-${qIndex}-${aIndex}`]
+                              .checked
+                          }
                           onChange={handleChange}
                           name={`explorationAnswer-${qIndex}-${aIndex}`}
+                          color="primary"
                         />
                       }
                       label={answer.text}
