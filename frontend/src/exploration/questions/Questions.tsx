6cc65a77e22ebcc29a024d7e07cf3da74b9e5fa2
derwehr <derwehr@gmail.com>
2021-06-10 19:58:07 +0200

    implement exploration page ui, closes #294

--- frontend/src/exploration/questions/Questions.tsx
+++ frontend/src/exploration/questions/Questions.tsx
@@ -1,20 +1,103 @@
 import React from 'react';
-import { Box, makeStyles } from '@material-ui/core';
-import { red } from '@material-ui/core/colors';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  makeStyles,
+  Theme,
+  Typography,
+} from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { createStyles } from '@material-ui/core/styles';
+import ExplorationStore from '../../stores/exploration/ExplorationStore';
+import useService from '../../dependency-injection/useService';
+import explorationQuestions from '../../stores/exploration/ExplorationQuestions';
+import { ExplorationAnswer } from '../../stores/exploration';
 
-const useStyle = makeStyles(() =>
+const useStyle = makeStyles((theme: Theme) =>
   createStyles({
-    container: {
-      backgroundColor: red[500], // TODO change/remove
-      height: '75vh', // TODO change/remove
+    heading: {
+      fontSize: theme.typography.pxToRem(15),
+      fontWeight: theme.typography.fontWeightRegular,
+    },
+    formControl: {
+      margin: theme.spacing(3),
     },
   })
 );
 
 function Questions(): JSX.Element {
   const classes = useStyle();
-  return <Box className={`${classes.container} Questions`}>TODO</Box>;
+
+  const explorationStore = useService(ExplorationStore);
+
+  // Map answers to string of questionIndex-answerIndex, in order to add and remove
+  // them from the explorationStore in handleChange
+  const answers: Record<string, ExplorationAnswer> = {};
+  explorationQuestions.forEach((q, qIndex) => {
+    q.answers.forEach((a, aIndex) => {
+      answers[`explorationAnswer-${qIndex}-${aIndex}`] = a;
+    });
+  });
+
+  /**
+   * Handles change event of the exploration questions,
+   * by adding newly selected answers and
+   * removing de-selected answers from the ExplorationStore.
+   * @param event the changeEvent
+   */
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name } = event.target;
+    const answer = answers[name];
+
+    if (event.target.checked) {
+      explorationStore.addAnswer(answer);
+    } else {
+      explorationStore.removeAnswer(answer);
+    }
+  };
+
+  return (
+    <>
+      <Box>
+        {explorationQuestions.map((question, qIndex) => (
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls={`question-${qIndex}-content`}
+              id={`question-${qIndex}-header`}
+            >
+              <Typography className={classes.heading}>
+                {question.text}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails id={`question-${qIndex}-content`}>
+              <FormControl className={classes.formControl}>
+                <FormGroup>
+                  {question.answers.map((answer, aIndex) => (
+                    <FormControlLabel
+                      control={
+                        <Checkbox
+                          onChange={handleChange}
+                          name={`explorationAnswer-${qIndex}-${aIndex}`}
+                        />
+                      }
+                      label={answer.text}
+                    />
+                  ))}
+                </FormGroup>
+              </FormControl>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+      </Box>
+    </>
+  );
 }
 
 export default Questions;
