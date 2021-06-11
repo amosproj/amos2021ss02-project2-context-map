cd03ee9c4cbba8713b78cfeebd09300edfbb249f
PianoRollRepresentation <kopytotobias@gmail.com>
2021-06-11 13:02:09 +0200

    Frontend add layouts to Previews.tsx

--- frontend/src/exploration/previews/Previews.tsx
+++ frontend/src/exploration/previews/Previews.tsx
@@ -1,20 +1,51 @@
 import React from 'react';
-import { Box, makeStyles } from '@material-ui/core';
-import { blue } from '@material-ui/core/colors';
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  makeStyles,
+  Paper,
+} from '@material-ui/core';
 import { createStyles } from '@material-ui/core/styles';
 
 const useStyle = makeStyles(() =>
   createStyles({
     container: {
-      backgroundColor: blue[500], // TODO change/remove
       height: '75vh', // TODO change/remove
+      width: '35vh',
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
     },
   })
 );
 
 function Previews(): JSX.Element {
   const classes = useStyle();
-  return <Box className={`${classes.container} Previews`}>TODO</Box>;
+  return (
+    <Box className={`${classes.container} Previews`}>
+      <Paper style={{ maxHeight: '100%', overflow: 'auto' }}>
+        <Card>
+          <CardMedia
+            className={classes.media}
+            image="/exploration-preview/structural layout.png"
+            title="Paella dish"
+          />
+          <CardContent>Structural Layout</CardContent>
+        </Card>
+        <Card>
+          <CardMedia
+            className={classes.media}
+            image="/exploration-preview/hierarchical layout.png"
+            title="Paella dish"
+          />
+          <CardContent>Hierarchical Layout</CardContent>
+        </Card>
+      </Paper>
+    </Box>
+  );
 }
 
 export default Previews;
