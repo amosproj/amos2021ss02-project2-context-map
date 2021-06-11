424a7ac21e37da78021a41b6016288fae61b47dc
PianoRollRepresentation <kopytotobias@gmail.com>
2021-06-11 14:06:11 +0200

    Frontend add Previews.tsx to new branch

--- frontend/src/exploration/previews/Previews.tsx
+++ frontend/src/exploration/previews/Previews.tsx
@@ -1,20 +1,72 @@
 import React from 'react';
-import { Box, makeStyles } from '@material-ui/core';
-import { blue } from '@material-ui/core/colors';
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  List,
+  ListItem,
+  makeStyles,
+  Paper,
+} from '@material-ui/core';
 import { createStyles } from '@material-ui/core/styles';
 
 const useStyle = makeStyles(() =>
   createStyles({
     container: {
-      backgroundColor: blue[500], // TODO change/remove
-      height: '75vh', // TODO change/remove
+      height: '75vh',
+      width: '35vh',
+    },
+    card: {
+      width: '100%',
+    },
+    media: {
+      height: 0,
+      paddingTop: '56.25%', // 16:9
     },
   })
 );
 
 function Previews(): JSX.Element {
   const classes = useStyle();
-  return <Box className={`${classes.container} Previews`}>TODO</Box>;
+  return (
+    <Box className={`${classes.container} Previews`}>
+      <Paper style={{ maxHeight: '100%', overflow: 'auto' }}>
+        <List>
+          <ListItem>
+            <Card className={classes.card}>
+              <CardMedia
+                className={classes.media}
+                image="/exploration-preview/structural layout.png"
+                title="Paella dish"
+              />
+              <CardContent>Structural Layout</CardContent>
+            </Card>
+          </ListItem>
+          <ListItem>
+            <Card className={classes.card}>
+              <CardMedia
+                className={classes.media}
+                image="/exploration-preview/hierarchical layout.png"
+                title="Paella dish"
+              />
+              <CardContent>Hierarchical Layout</CardContent>
+            </Card>
+          </ListItem>
+          <ListItem>
+            <Card className={classes.card}>
+              <CardMedia
+                className={classes.media}
+                image="/exploration-preview/empty layout.png"
+                title="Paella dish"
+              />
+              <CardContent>Empty Layout</CardContent>
+            </Card>
+          </ListItem>
+        </List>
+      </Paper>
+    </Box>
+  );
 }
 
 export default Previews;
