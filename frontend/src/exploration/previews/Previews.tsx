c22db6572fc31efe41dbd597c436fdc065a9f424
PianoRollRepresentation <kopytotobias@gmail.com>
2021-06-11 13:59:09 +0200

    Revert "Frontend move cards to list elements in Previews.tsx"
    This reverts commit c2e1f7a2

--- frontend/src/exploration/previews/Previews.tsx
+++ frontend/src/exploration/previews/Previews.tsx
@@ -4,8 +4,6 @@ import {
   Card,
   CardContent,
   CardMedia,
-  List,
-  ListItem,
   makeStyles,
   Paper,
 } from '@material-ui/core';
@@ -14,11 +12,8 @@ import { createStyles } from '@material-ui/core/styles';
 const useStyle = makeStyles(() =>
   createStyles({
     container: {
-      height: '75vh',
+      height: '75vh', // TODO change/remove
       width: '35vh',
-    },
-    card: {
-      width: '100%',
     },
     media: {
       height: 0,
@@ -32,38 +27,22 @@ function Previews(): JSX.Element {
   return (
     <Box className={`${classes.container} Previews`}>
       <Paper style={{ maxHeight: '100%', overflow: 'auto' }}>
-        <List>
-          <ListItem>
-            <Card className={classes.card}>
-              <CardMedia
-                className={classes.media}
-                image="/exploration-preview/structural layout.png"
-                title="Paella dish"
-              />
-              <CardContent>Structural Layout</CardContent>
-            </Card>
-          </ListItem>
-          <ListItem>
-            <Card className={classes.card}>
-              <CardMedia
-                className={classes.media}
-                image="/exploration-preview/hierarchical layout.png"
-                title="Paella dish"
-              />
-              <CardContent>Hierarchical Layout</CardContent>
-            </Card>
-          </ListItem>
-          <ListItem>
-            <Card className={classes.card}>
-              <CardMedia
-                className={classes.media}
-                image="/exploration-preview/empty layout.png"
-                title="Paella dish"
-              />
-              <CardContent>Empty Layout</CardContent>
-            </Card>
-          </ListItem>
-        </List>
+        <Card>
+          <CardMedia
+            className={classes.media}
+            image="/exploration-preview/structural layout.png"
+            title="Paella dish"
+          />
+          <CardContent>Structural Layout</CardContent>
+        </Card>
+        <Card>
+          <CardMedia
+            className={classes.media}
+            image="/exploration-preview/hierarchical layout.png"
+            title="Paella dish"
+          />
+          <CardContent>Hierarchical Layout</CardContent>
+        </Card>
       </Paper>
     </Box>
   );
