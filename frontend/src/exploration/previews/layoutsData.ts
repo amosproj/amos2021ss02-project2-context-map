5a268219771606c189f0539cef6ec624cce603c3
PianoRollRepresentation <kopytotobias@gmail.com>
2021-06-12 12:28:57 +0200

    Frontend add method that adds routing information to LayoutCard in layoutsData.ts

--- frontend/src/exploration/previews/layoutsData.ts
+++ frontend/src/exploration/previews/layoutsData.ts
@@ -1,23 +1,78 @@
-type LayoutData = { text: string; description: string; weight: number };
+import TabDefinition from '../../routing/TabDefinition';
+import LayoutDefinition from './LayoutDefinition';
+
+type LayoutData = {
+  label: string;
+  filename: string;
+  description: string;
+  weight: number;
+};
 
 export const layoutsData: {
   [key: string]: LayoutData;
 } = {
-  C: { text: 'structural layout', description: 'Structural Layout', weight: 0 },
+  C: {
+    label: 'Graph',
+    filename: 'structural layout',
+    description: 'Structural Layout',
+    weight: 0,
+  },
   BC: {
-    text: 'empty layout',
+    label: 'Schema', // TODO: change when Betweenness Centrality is added
+    filename: 'empty layout',
     description: 'Betweenness Centrality',
     weight: 0,
   },
   H: {
-    text: 'hierarchical layout',
+    label: 'Hierarchies',
+    filename: 'hierarchical layout',
     description: 'Hierarchical Layout',
     weight: 0,
   },
-  R: { text: 'empty layout', description: 'Radial Layout', weight: 0 },
-  SP: { text: 'empty layout', description: 'Shortest Path', weight: 0 },
-  L: { text: 'empty layout', description: 'List Layout', weight: 0 },
-  P: { text: 'empty layout', description: 'Pie Chart', weight: 0 },
+  R: {
+    label: 'Schema', // TODO: change when Radial Layout is added
+    filename: 'empty layout',
+    description: 'Radial Layout',
+    weight: 0,
+  },
+  SP: {
+    label: 'Graph',
+    filename: 'empty layout',
+    description: 'Shortest Path',
+    weight: 0,
+  },
+  L: {
+    label: 'Schema', // TODO: change when List Layout is added
+    filename: 'empty layout',
+    description: 'List Layout',
+    weight: 0,
+  },
+  P: {
+    label: 'Schema', // TODO: change when Pie Chart is added
+    filename: 'empty layout',
+    description: 'Pie Chart',
+    weight: 0,
+  },
 };
 
-export default layoutsData;
+const createLayoutCard = (
+  layoutData: LayoutData,
+  tabs: TabDefinition[]
+): LayoutDefinition => {
+  const tab = tabs.find((curTab) => curTab.label === layoutData.label);
+
+  /* istanbul ignore if */
+  if (tab === undefined) {
+    throw new Error(`No tab found to label ${layoutData.label}`);
+  }
+
+  return {
+    label: tab.label,
+    path: tab.path,
+    content: tab.content,
+    description: layoutData.description,
+    filename: layoutData.filename,
+  };
+};
+
+export default createLayoutCard;
