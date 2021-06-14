e7864168fdddd948e22aa28cd3d42ee848e796e9
PianoRollRepresentation <kopytotobias@gmail.com>
2021-06-14 09:55:26 +0200

    Frontend add tests in exploration.e2e.js

--- frontend/cypress/integration/exploration/exploration.e2e.js
+++ frontend/cypress/integration/exploration/exploration.e2e.js
@@ -1,3 +1,5 @@
+import layoutsData from '../../../src/exploration/previews/layoutsData';
+
 context('Exploration', () => {
   // Global setup
   beforeEach(() => {
@@ -9,6 +11,28 @@ context('Exploration', () => {
       cy.get('.Previews');
       cy.get('.LayoutPreview').should('have.length', 7);
     });
+
+    it('correct initial layouts', () => {
+      cy.get('.Previews');
+
+      const layoutsDataValues = Object.values(layoutsData);
+      cy.get('.LayoutPreview').each(($el, index) => {
+        cy.wrap($el).should('have.text', layoutsDataValues[index].description);
+      });
+    });
+
+    it('routes to graph page', () => {
+      cy.get('.Previews');
+      cy.get('.LayoutPreview').eq(0).click();
+      cy.url().should('eq', `http://localhost:3000${layoutsData.C.path}`);
+    });
+
+    it('routes to hierarchical page', () => {
+      cy.get('.Previews');
+      cy.get('.LayoutPreview').eq(2).click();
+      cy.url().should('eq', `http://localhost:3000${layoutsData.H.path}`);
+    });
+
     // TODO: add more tests when combined with questions
   });
 });
