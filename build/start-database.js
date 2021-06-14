51b8b2378db814aeb32e3e67807dd277edb0f115
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2021-06-14 13:06:52 +0200

    unifies start-database script for all OS'

---
+++ build/start-database.js
@@ -0,0 +1,37 @@
+
+const path = require("path");
+const { execSync } = require("child_process");
+
+const containerName="neo4j-db"
+const repoPath = path.join(__dirname, "..");
+const mountPath = path.join(repoPath, "backend", "docker", "mount");
+const pluginsPath = path.join(repoPath, "backend", "docker", "plugins");
+
+async function main() {
+  try {
+    execSync(`docker container stop ${containerName}`)
+  } catch(e) {}
+
+  try {
+    execSync(`docker container rm ${containerName}`)
+  } catch(e) {}
+
+  const command = `
+    docker run -d -p 7474:7474 -p 7687:7687 \\
+      -v ${mountPath}:/mnt/amos \\
+      -v ${pluginsPath}:/plugins \\
+      --env NEO4J_ACCEPT_LICENSE_AGREEMENT=yes \\
+      --env DB_PASSWORD=amos \\
+      --env DB_PATH=/mnt/amos/dumps/testing-dump.dump \\
+      --env 'NEO4JLABS_PLUGINS=["apoc", "graph-data-science"]' \\
+      --env 'NEO4J_dbms_security_procedures_unrestricted=apoc.*,gds.*' \\
+      --env 'NEO4J_dbms_security_procedures_allowlist=apoc.*,gds.*' \\
+      --env NEO4J_apoc_import_file_enabled=true \\
+      --name ${containerName} neo4j:enterprise \\
+      /mnt/amos/load-dump.sh
+`
+
+  execSync(command);
+}
+
+main();
