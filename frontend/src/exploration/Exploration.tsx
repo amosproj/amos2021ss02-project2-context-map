d7879edfab152fe8e81a08e8eb71bb1f486891f7
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2021-06-15 14:57:25 +0200

    changes exploration layout

--- frontend/src/exploration/Exploration.tsx
+++ frontend/src/exploration/Exploration.tsx
@@ -1,38 +1,56 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { Box, makeStyles } from '@material-ui/core';
-import { createStyles } from '@material-ui/core/styles';
+import { createStyles, Theme } from '@material-ui/core/styles';
+import useResizeObserver from '@react-hook/resize-observer';
 import Previews from './previews/Previews';
 import Questions from './questions/Questions';
 
-const useStyle = makeStyles((theme) =>
+type ThemeProps = {
+  previewHeaderHeight: number;
+};
+
+const useStyle = makeStyles<Theme, ThemeProps>((theme) =>
   createStyles({
     container: {
-      margin: theme.spacing(4),
+      margin: theme.spacing(10),
+      // top margin should be a bit smaller
+      marginTop: theme.spacing(5),
       gap: theme.spacing(4),
     },
-    preview: {
-      height: '66vh',
-    },
+    questions: (props) => ({
+      marginTop: props.previewHeaderHeight,
+    }),
   })
 );
 
 function Exploration(): JSX.Element {
-  const classes = useStyle();
+  // ref to the main content container
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  // height of the header of the preview
+  const [previewHeaderHeight, setPreviewHeaderHeight] = useState<number>(0);
+
+  useResizeObserver(containerRef, () => {
+    setPreviewHeaderHeight(
+      document.querySelector('.Previews h1')?.getBoundingClientRect().height ??
+        0
+    );
+  });
+
+  const classes = useStyle({ previewHeaderHeight });
   return (
     <>
-      <h1>Exploration</h1>
-      <Box
-        className={classes.container}
-        display="flex"
-        justifyContent="space-around"
-      >
-        <Box flex={3}>
-          <Questions />
+      <h1 id="ExplorationHeader">Exploration</h1>
+      <div ref={containerRef}>
+        <Box className={classes.container} display="flex">
+          <Box flex={3} className={classes.questions}>
+            <Questions />
+          </Box>
+          <Box flex={2}>
+            <Previews />
+          </Box>
         </Box>
-        <Box flex={2} className={classes.preview}>
-          <Previews />
-        </Box>
-      </Box>
+      </div>
     </>
   );
 }
