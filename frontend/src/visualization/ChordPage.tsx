4aaab61744e67de9f5a00e0f0127375fb0e1f5da
derwehr <derwehr@gmail.com>
2021-06-18 13:46:42 +0200

    remove unsused styles

--- frontend/src/visualization/ChordPage.tsx
+++ frontend/src/visualization/ChordPage.tsx
@@ -1,17 +1,4 @@
 import React from 'react';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles(() =>
-  createStyles({
-    graphContainer: {
-      zIndex: 1200,
-      position: 'relative',
-      flexGrow: 1,
-      overflowY: 'hidden',
-      overflowX: 'hidden',
-    },
-  })
-);
 
 function ChordPage(): JSX.Element {
   return <></>;
