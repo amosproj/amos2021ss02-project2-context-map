9b3daa4c43451a7d297f329de2998cf929199728
PianoRollRepresentation <kopytotobias@gmail.com>
2021-06-20 10:14:11 +0200

    Shared delete identifier in NodeTypeDescriptor.ts and EdgeTypeDescriptor.ts

--- shared/src/schema/EdgeTypeDescriptor.ts
+++ shared/src/schema/EdgeTypeDescriptor.ts
@@ -4,9 +4,4 @@ import { EntityTypeDescriptor } from './EntityTypeDescriptor';
  * A descriptor of an edge type.
  */
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface EdgeTypeDescriptor extends EntityTypeDescriptor {
-  /**
-   * Used to identify if an object of this type corresponds to an EdgeTypeDescriptor
-   */
-  edge: undefined;
-}
+export interface EdgeTypeDescriptor extends EntityTypeDescriptor {}
--- shared/src/schema/NodeTypeDescriptor.ts
+++ shared/src/schema/NodeTypeDescriptor.ts
@@ -4,9 +4,4 @@ import { EntityTypeDescriptor } from './EntityTypeDescriptor';
  * A descriptor of a node type.
  */
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface NodeTypeDescriptor extends EntityTypeDescriptor {
-  /**
-   * Used to identify if an object of this type corresponds to an NodeTypeDescriptor
-   */
-  node: undefined;
-}
+export interface NodeTypeDescriptor extends EntityTypeDescriptor {}
