2de8349533fef74d794d6e5d5f531132bc791be2
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2021-06-22 10:15:56 +0200

    make ShortestPathService use the new HttpService

--- frontend/src/services/shortest-path/ShortestPathServiceImpl.ts
+++ frontend/src/services/shortest-path/ShortestPathServiceImpl.ts
@@ -1,7 +1,7 @@
 import { injectable, inject } from 'inversify';
 import 'reflect-metadata';
+import { Observable } from 'rxjs';
 import { QueryResult, ShortestPathQuery } from '../../shared/queries';
-import { CancellationToken } from '../../utils/CancellationToken';
 import HttpService from '../http';
 import ShortestPathService from './ShortestPathService';
 
@@ -10,14 +10,7 @@ export default class ShortestPathServiceImpl implements ShortestPathService {
   @inject(HttpService)
   private readonly http!: HttpService;
 
-  public query(
-    query?: ShortestPathQuery,
-    cancellation?: CancellationToken
-  ): Promise<QueryResult> {
-    return this.http.post<QueryResult>(
-      '/api/shortest-path/query',
-      query,
-      cancellation
-    );
+  public query(query?: ShortestPathQuery): Observable<QueryResult> {
+    return this.http.post<QueryResult>('/api/shortest-path/query', query);
   }
 }
