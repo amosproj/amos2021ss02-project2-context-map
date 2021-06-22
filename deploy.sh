166e46eca2b913b7d3acb7e47dfc77ff19108de8
PianoRollRepresentation <kopytotobias@gmail.com>
2021-06-22 11:15:36 +0200

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,35 @@
+CopyFilesToFolder () {
+  fromFolder=$1
+  toFolder=$2
+  cp -a $fromFolder $toFolder
+}
+
+# Deploy the backend to a docker container
+# First copy the shared files, as this is not done during the build process when running in docker
+cp -a ./shared/src/. ./backend/src/shared/
+
+# Now build the container as spec'ed by the backend dockerfile
+docker build -t kmap.backend ./backend
+
+# Deploy the frontend to a docker container that runs nginx
+# First copy the shared files, as this is not done during the build process when running in docker
+cp -a ./shared/src/. ./frontend/src/shared/
+
+# Remove the .env file, if it is present.
+if test -f ./frontend/.env; then
+  rm ./frontend/.env
+fi
+
+# Now build the container as spec'ed by the backend dockerfile
+docker build -t kmap.frontend ./frontend
+
+# Compose the output folder
+if test -d ./artifacts/; then
+  rm -rf ./artifacts/
+fi
+
+CopyFilesToFolder ./deploy/ ./artifacts/
+CopyFilesToFolder ./database/ ./artifacts/database/
+
+docker save -o .\artifacts\images\kmap.backend kmap.backend
+docker save -o .\artifacts\images\kmap.frontend kmap.frontend
