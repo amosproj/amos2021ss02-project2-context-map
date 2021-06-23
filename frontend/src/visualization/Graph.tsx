4893733902e89dbabddd3e23e2c0cc248b5f0b39
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2021-06-23 14:03:37 +0200

    add warning if search selection not in graph

--- frontend/src/visualization/Graph.tsx
+++ frontend/src/visualization/Graph.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import VisGraph from 'react-graph-vis';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import { uuid } from 'uuidv4';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 import { combineLatest } from 'rxjs';
+import { Snackbar } from '@material-ui/core';
+import { Alert } from '@material-ui/lab';
 import useService from '../dependency-injection/useService';
 import { ContainerSize } from '../utils/useSize';
 import useObservable from '../utils/useObservable';
 import QueryResultStore from '../stores/QueryResultStore';
 import convertQueryResult from './shared-ops/convertQueryResult';
-import { EntityStyleStore } from '../stores/colors';
+import { createSelectionInfo, EntityStyleStore } from '../stores/colors';
+import SearchSelectionStore from '../stores/SearchSelectionStore';
+import { isEntitySelected } from '../stores/colors/EntityStyleProviderImpl';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -52,8 +56,16 @@ function Graph(props: GraphProps): JSX.Element {
   const { layout, containerSize } = props;
   const classes = useStyles();
 
+  // whether snackbar with hint that selected entity is not found is open or not
+  const [noEntitiesFoundWarningOpen, setNoEntitiesFoundWarningOpen] =
+    useState(false);
+  const onNoEntitiesFoundWarningSnackbarClose = () => {
+    setNoEntitiesFoundWarningOpen(false);
+  };
+
   const queryResultStore = useService(QueryResultStore);
   const entityColorStore = useService(EntityStyleStore);
+  const searchSelectionStore = useService(SearchSelectionStore);
 
   const graphData = useObservable(
     // When one emits, the whole observable emits with the last emitted value from the other inputs
@@ -63,6 +75,26 @@ function Graph(props: GraphProps): JSX.Element {
       entityColorStore.getState(),
     ]).pipe(map((next) => convertQueryResult(next[0], next[1]))),
     { edges: [], nodes: [] }
+  );
+
+  // When either the query result or the selected entity changes => check if
+  // selection is in query result.
+  useObservable(
+    combineLatest([
+      queryResultStore.getState(),
+      searchSelectionStore.getState(),
+    ]).pipe(
+      tap(([queryResult, selection]) => {
+        if (selection === undefined) return;
+        const selectionInfo = createSelectionInfo(selection);
+        const entityFound =
+          queryResult.edges.some((e) => isEntitySelected(e, selectionInfo)) ||
+          queryResult.nodes.some((n) => isEntitySelected(n, selectionInfo));
+        if (!entityFound) {
+          setNoEntitiesFoundWarningOpen(true);
+        }
+      })
+    )
   );
 
   return (
@@ -78,6 +110,16 @@ function Graph(props: GraphProps): JSX.Element {
           key={uuid()}
         />
       </div>
+      <Snackbar
+        open={noEntitiesFoundWarningOpen}
+        autoHideDuration={4000}
+        onClose={onNoEntitiesFoundWarningSnackbarClose}
+        anchorOrigin={{ horizontal: 'left', vertical: 'bottom' }}
+      >
+        <Alert severity="warning">
+          Selected entity not found in the displayed graph.
+        </Alert>
+      </Snackbar>
     </>
   );
 }
