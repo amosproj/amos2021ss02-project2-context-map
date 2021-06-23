42673a4b50e90115999a206ffe458dfd44586aa0
derwehr <derwehr@gmail.com>
2021-06-23 17:18:56 +0200

    remove schema page

--- frontend/cypress/integration/visualization/tabs.e2e.js
+++ frontend/cypress/integration/visualization/tabs.e2e.js
@@ -20,11 +20,6 @@ context('Visualization Tabs', () => {
     cy.get('main').get('.vis-network');
   });
 
-  it('has schema tab', () => {
-    cy.get('.MuiTabs-root').contains('Schema').click();
-    cy.get('main').contains('Schema');
-  });
-
   it('has hierarchies tab', () => {
     cy.get('.MuiTabs-root').contains('Hierarchies').click();
     cy.get('main').get('.vis-network');
