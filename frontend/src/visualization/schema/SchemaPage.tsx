39f52284ef6c3eaac61d6dfa0ca876432fc0276e
derwehr <derwehr@gmail.com>
2021-06-23 16:23:07 +0200

    add schema page

---
+++ frontend/src/visualization/schema/SchemaPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function SchemaPage(): JSX.Element {
+  return (
+    <>
+      <h1>Schema</h1>
+      <p>Coming soon...</p>
+      <div style={{ width: '100%', textAlign: 'center' }}>
+        <img
+          src="/exploration-preview/schema.png"
+          alt="Schema"
+          style={{ maxWidth: '100%', maxHeight: '75vh' }}
+        />
+      </div>
+    </>
+  );
+}
