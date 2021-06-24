41e627adae5559120c3f437852d22b110ac81ec0
PianoRollRepresentation <kopytotobias@gmail.com>
2021-06-24 12:16:57 +0200

    Frontend move methods from FilterStateStore.ts to FilterState.ts

--- frontend/src/stores/filterState/FilterState.ts
+++ frontend/src/stores/filterState/FilterState.ts
@@ -36,14 +36,159 @@ export interface FilterLineState {
 /**
  * State of a filter
  */
-export interface FilterState {
-  /**
-   * States of the filter lines of the nodes
-   */
-  edges: FilterLineState[];
+export class FilterState {
+  constructor(
+    private _edges: FilterLineState[],
+    private _nodes: FilterLineState[]
+  ) {}
+
+  get edges(): FilterLineState[] {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._edges;
+  }
+
+  get nodes(): FilterLineState[] {
+    // eslint-disable-next-line no-underscore-dangle
+    return this._nodes;
+  }
 
   /**
-   * States of the filter lines of the edges
+   * Gets the isActive value of a {@link FilterLineState}.
+   * @param filterLineType - filter line which is searched for
+   * @param entity - specifies whether this entity is a node or an edge
    */
-  nodes: FilterLineState[];
+  public getFilterLineIsActive(
+    filterLineType: string,
+    entity: 'node' | 'edge'
+  ): boolean {
+    const lineState: FilterLineState | undefined = this.getLineFromFilterState(
+      filterLineType,
+      entity
+    );
+
+    return lineState !== undefined ? lineState.isActive : false;
+  }
+
+  /**
+   * Toggles the {@link isActive} property of the specified filter line.
+   * @param filterLineType - filter line which {@link isActive} property is toggled
+   * @param entity - specifies whether this entity is a node or an edge
+   */
+  public toggleFilterLineActive(
+    filterLineType: string,
+    entity: 'node' | 'edge'
+  ): void {
+    const line: FilterLineState | undefined = this.getLineFromFilterState(
+      filterLineType,
+      entity
+    );
+
+    if (line) {
+      line.isActive = !line.isActive;
+    }
+  }
+
+  /**
+   * Sets the {@link isActive} property of the specified filter line active.
+   * @param filterLineType - filter line which {@link isActive} property is set active
+   * @param entity - specifies whether this entity is a node or an edge
+   */
+  public setFilterLineActive(
+    filterLineType: string,
+    entity: 'node' | 'edge'
+  ): void {
+    const line: FilterLineState | undefined = this.getLineFromFilterState(
+      filterLineType,
+      entity
+    );
+
+    if (line) {
+      line.isActive = true;
+    }
+  }
+
+  /**
+   * Adds a new {@link FilterPropertyState} to the specified filter line. If there already
+   * exists a {@link FilterPropertyState} with the given name in the filter line, the
+   * {@link FilterPropertyState} is overwritten with the new one.
+   * @param stateToAdd - the {@link FilterPropertyState} to be added
+   * @param filterLineType - filter line which the {@link FilterPropertyState} is added to
+   * @param entity - specifies whether this entity is a node or an edge
+   */
+  public addFilterPropertyState(
+    stateToAdd: FilterPropertyState,
+    filterLineType: string,
+    entity: 'node' | 'edge'
+  ): void {
+    const lineState: FilterLineState | undefined = this.getLineFromFilterState(
+      filterLineType,
+      entity
+    );
+
+    if (lineState) {
+      const propertyWithExistentName = lineState.propertyFilters.find(
+        (e) => e.name === stateToAdd.name
+      );
+      if (propertyWithExistentName) {
+        propertyWithExistentName.values = stateToAdd.values;
+      } else {
+        lineState.propertyFilters.push(stateToAdd);
+      }
+    }
+  }
+
+  /**
+   * Gets the values of a {@link FilterPropertyState} in the specified filter line if they exist.
+   * Otherwise undefined is returned.
+   * @param filterLineType - filter line which is searched for
+   * @param filterPropertyName - name of the {@link FilterPropertyState}
+   * @param entity - specifies whether this entity is a node or an edge
+   */
+  public getPropertyStateValues(
+    filterLineType: string,
+    filterPropertyName: string,
+    entity: 'node' | 'edge'
+  ): string[] | undefined {
+    const propertyState: FilterPropertyState | undefined =
+      this.searchForPropertyState(filterLineType, filterPropertyName, entity);
+
+    return propertyState?.values;
+  }
+
+  private searchForPropertyState(
+    filterLineType: string,
+    filterPropertyName: string,
+    entity: 'node' | 'edge'
+  ) {
+    const line: FilterLineState | undefined = this.getLineFromFilterState(
+      filterLineType,
+      entity
+    );
+
+    if (line) {
+      for (const propertyFilters of line.propertyFilters) {
+        if (propertyFilters.name === filterPropertyName) {
+          return propertyFilters;
+        }
+      }
+    }
+
+    return undefined;
+  }
+
+  private getLineFromFilterState(
+    filterLineType: string,
+    entity: 'node' | 'edge'
+  ): FilterLineState | undefined {
+    const entityLineStates = entity === 'node' ? this.nodes : this.edges;
+
+    for (const entityLine of entityLineStates) {
+      if (entityLine.type === filterLineType) {
+        return entityLine;
+      }
+    }
+
+    /* istanbul ignore next */
+    return undefined;
+  }
 }
--- frontend/src/stores/filterState/FilterStateStore.ts
+++ frontend/src/stores/filterState/FilterStateStore.ts
@@ -1,11 +1,7 @@
 /* istanbul ignore file */
 import { injectable } from 'inversify';
 import SimpleStore from '../SimpleStore';
-import {
-  FilterLineState,
-  FilterPropertyState,
-  FilterState,
-} from './FilterState';
+import { FilterPropertyState, FilterState } from './FilterState';
 
 /**
  * Contains the current state of the filter.
@@ -13,116 +9,51 @@ import {
 @injectable()
 export default class FilterStateStore extends SimpleStore<FilterState> {
   protected getInitialValue(): FilterState {
-    return { edges: [], nodes: [] };
+    return new FilterState([], []);
   }
 
-  /**
-   * Toggles the {@link isActive} property of the specified filter line.
-   * @param filterLineType - filter line which {@link isActive} property is toggled
-   * @param entity - specifies whether this entity is a node or an edge
-   */
+  public getValue(): FilterState {
+    const json = super.getValue();
+    return new FilterState(json.edges, json.nodes);
+  }
+
   public toggleFilterLineActive(
     filterLineType: string,
     entity: 'node' | 'edge'
   ): void {
-    const line: FilterLineState | undefined = this.getLineFromFilterState(
-      filterLineType,
-      entity
-    );
-
-    if (line) {
-      line.isActive = !line.isActive;
-    }
+    this.getValue().toggleFilterLineActive(filterLineType, entity);
 
     this.setState(this.getValue());
   }
 
-  /**
-   * Adds a new {@link FilterPropertyState} to the specified filter line. If there already
-   * exists a {@link FilterPropertyState} with the given name in the filter line, the
-   * {@link FilterPropertyState} is overwritten with the new one.
-   * @param stateToAdd - the {@link FilterPropertyState} to be added
-   * @param filterLineType - filter line which the {@link FilterPropertyState} is added to
-   * @param entity - specifies whether this entity is a node or an edge
-   */
+  public setFilterLineActive(
+    filterLineType: string,
+    entity: 'node' | 'edge'
+  ): void {
+    this.getValue().setFilterLineActive(filterLineType, entity);
+
+    this.setState(this.getValue());
+  }
+
   public addFilterPropertyState(
     stateToAdd: FilterPropertyState,
     filterLineType: string,
     entity: 'node' | 'edge'
   ): void {
-    const lineState: FilterLineState | undefined = this.getLineFromFilterState(
-      filterLineType,
-      entity
-    );
-
-    if (lineState) {
-      const propertyWithExistentName = lineState.propertyFilters.find(
-        (e) => e.name === stateToAdd.name
-      );
-      if (propertyWithExistentName) {
-        propertyWithExistentName.values = stateToAdd.values;
-      } else {
-        lineState.propertyFilters.push(stateToAdd);
-      }
-    }
+    this.getValue().addFilterPropertyState(stateToAdd, filterLineType, entity);
 
     this.setState(this.getValue());
   }
 
-  /**
-   * Gets the values of a {@link FilterPropertyState} in the specified filter line if they exist.
-   * Otherwise undefined is returned.
-   * @param filterLineType - filter line which is searched for
-   * @param filterPropertyName - name of the {@link FilterPropertyState}
-   * @param entity - specifies whether this entity is a node or an edge
-   */
   public getPropertyStateValues(
     filterLineType: string,
     filterPropertyName: string,
     entity: 'node' | 'edge'
   ): string[] | undefined {
-    const propertyState: FilterPropertyState | undefined =
-      this.searchForPropertyState(filterLineType, filterPropertyName, entity);
-
-    return propertyState?.values;
-  }
-
-  private getLineFromFilterState(
-    filterLineType: string,
-    entity: 'node' | 'edge'
-  ): FilterLineState | undefined {
-    const filterState = this.getValue();
-    const entityLineStates =
-      entity === 'node' ? filterState.nodes : filterState.edges;
-
-    for (const entityLine of entityLineStates) {
-      if (entityLine.type === filterLineType) {
-        return entityLine;
-      }
-    }
-
-    /* istanbul ignore next */
-    return undefined;
-  }
-
-  private searchForPropertyState(
-    filterLineType: string,
-    filterPropertyName: string,
-    entity: 'node' | 'edge'
-  ) {
-    const line: FilterLineState | undefined = this.getLineFromFilterState(
+    return this.getValue().getPropertyStateValues(
       filterLineType,
+      filterPropertyName,
       entity
     );
-
-    if (line) {
-      for (const propertyFilters of line.propertyFilters) {
-        if (propertyFilters.name === filterPropertyName) {
-          return propertyFilters;
-        }
-      }
-    }
-
-    return undefined;
   }
 }
