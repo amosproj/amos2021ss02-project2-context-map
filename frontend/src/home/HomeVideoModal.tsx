0240305cf33f08d4f1c41bb7d647f9478bac8346
derwehr <derwehr@gmail.com>
2021-06-25 13:45:58 +0200

    clean up

--- frontend/src/home/HomeVideoModal.tsx
+++ frontend/src/home/HomeVideoModal.tsx
@@ -1,15 +1,9 @@
 import React from 'react';
 import Modal from '@material-ui/core/Modal';
-import {
-  Button,
-  createStyles,
-  Dialog,
-  makeStyles,
-  Theme,
-} from '@material-ui/core';
+import { Button, createStyles, Dialog, makeStyles } from '@material-ui/core';
 import { deepPurple, purple } from '@material-ui/core/colors';
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
     button: {
       color: '#fff',
@@ -19,13 +13,8 @@ const useStyles = makeStyles((theme: Theme) =>
       },
     },
     iframe: {
-      position: 'fixed',
-      right: 0,
-      bottom: 0,
-      minWidth: '100%',
-      minHeight: '100%',
-      width: 'auto',
-      height: 'auto',
+      width: '1280',
+      height: '720',
     },
   })
 );
@@ -47,12 +36,12 @@ function HomeVideoModal(): JSX.Element {
       {/* youtube iFrame */}
       <iframe
         className={classes.iframe}
-        width="560"
-        height="315"
+        width="1280"
+        height="720"
         src="https://www.youtube.com/embed/dQw4w9WgXcQ"
         title="YouTube video player"
         frameBorder="0"
-        allow="accelerometer; fullscreen; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
       />
     </Dialog>
