6137ee8d29704644919b9fe44a5ee15a83d9aaf3
PianoRollRepresentation <kopytotobias@gmail.com>
2021-07-02 10:59:43 +0200

    Frontend change initial nodes in EntityCountsStore.ts to 150

--- frontend/src/stores/EntityCountsStore.ts
+++ frontend/src/stores/EntityCountsStore.ts
@@ -5,7 +5,7 @@ type EntityCounts = { nodes: number; edges: number };
 
 export default class EntityCountsStore extends SimpleStore<EntityCounts> {
   protected getInitialValue(): CountQueryResult {
-    return { nodes: 1, edges: 150 };
+    return { nodes: 150, edges: 150 };
   }
 
   public reset(): void {
