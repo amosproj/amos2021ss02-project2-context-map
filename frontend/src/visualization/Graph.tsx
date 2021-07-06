a8e20f45aff967bce4a38811ffa4d5e7eeec7eeb
Johannes Jablonski <johannes.lukas.jablonski@gmail.com>
2021-07-06 10:37:33 +0200

    remove unused variable

--- frontend/src/visualization/Graph.tsx
+++ frontend/src/visualization/Graph.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import VisGraph from 'react-graph-vis';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import { uuid } from 'uuidv4';
